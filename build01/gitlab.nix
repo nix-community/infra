756b4cc38f1fd66d837200d10d1f90fcb26ee209
zimbatm <zimbatm@zimbatm.com>
2019-12-18 23:29:08 +0100

    build01/gitlab: fix infinite recursion
    I don't know how this was working. The `pkgs` attribute is only
    available after all the `imports` are done.

--- build01/gitlab.nix
+++ build01/gitlab.nix
@@ -1,13 +1,10 @@
 { pkgs, ... }:
 
 let
-  gitlabModule = pkgs.fetchFromGitLab {
-    owner = "arianvp";
-    repo = "nixos-gitlab-runner";
-    rev = "9126927c701aa399bd1734e7e5230c3a0010c1b7";
+  gitlabModule = builtins.fetchTarball {
+    url = "https://gitlab.com/arianvp/nixos-gitlab-runner/-/archive/9126927c701aa399bd1734e7e5230c3a0010c1b7/nixos-gitlab-runner-9126927c701aa399bd1734e7e5230c3a0010c1b7.tar.gz";
     sha256 = "1s0fy5ny2ygcfvx35xws8xz5ih4z4kdfqlq3r6byxpylw7r52fyi";
   };
-
 in {
   imports = [
     "${gitlabModule}/gitlab-runner.nix"
