c989fbe2c7fefb52835418874e0f05e1d9e40c0c
zimbatm <zimbatm@zimbatm.com>
2020-01-12 14:03:14 +0100

    nixos: set default deployment and state in the shell
    This makes it easier to play with nixops. Eg: `nixops ssh build01`

--- shell.nix
+++ shell.nix
@@ -10,9 +10,11 @@ in pkgs.mkShell {
 
   NIX_PATH="nixpkgs=${toString pkgs.path}";
 
+  NIXOPS_DEPLOYMENT="nix-community-infra";
+  NIXOPS_STATE="./state/deployment-state.nixops";
+
   buildInputs = [
     pkgs.git-crypt
     pkgs.nixops
   ];
-
 }
