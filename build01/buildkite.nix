3e2ae4a579460ec04ee793e6faba375f86274ee8
zimbatm <zimbatm@zimbatm.com>
2020-01-12 13:53:27 +0100

    secrets: read all files from dir
    this enforces 1:1 mapping between the filename and the attribute name

--- build01/buildkite.nix
+++ build01/buildkite.nix
@@ -5,7 +5,7 @@
     enable = true;
     tokenPath = "/run/keys/buildkite-token";
     openssh.privateKeyPath = builtins.toPath "/run/keys/buildkite-agent-key";
-    openssh.publicKeyPath = builtins.toPath "/run/keys/buildkite-agent-key-pub";
+    openssh.publicKeyPath = builtins.toPath "/run/keys/buildkite-agent-key.pub";
 
     runtimePackages = [
       pkgs.gnutar
