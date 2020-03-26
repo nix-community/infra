a53c2c41a32969d4458551fb2039594d162eee7a
Florian Klink <flokli@flokli.de>
2020-03-26 12:04:42 +0100

    add flokli (#11)

---
+++ users/flokli.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, lib, ... }:
+
+let
+  userLib = import ./lib.nix { inherit lib; };
+  keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPTVTXOutUZZjXLB0lUSgeKcSY/8mxKkC0ingGK1whD2 flokli"
+  ];
+in
+{
+  users.users.flokli = {
+    openssh.authorizedKeys.keys = keys;
+    useDefaultShell = true;
+    isNormalUser = true;
+    extraGroups = [
+      "wheel"
+    ];
+    uid = userLib.mkUid "flokli";
+  };
+
+  nix.trustedUsers = [ "flokli" ];
+}
