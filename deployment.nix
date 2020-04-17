0c018ccc3545febb3a81120225a612eb978461e8
zimbatm <zimbatm@zimbatm.com>
2020-04-17 22:54:03 +0200

    fix buildkite agent keys permission

--- deployment.nix
+++ deployment.nix
@@ -18,19 +18,19 @@ in
 
         deployment.keys.buildkite-token = {
           text = secrets.buildkite-token;
-          user = "buildkite-agent";
+          user = "buildkite-agent-ci";
           permissions = "0600";
         };
 
         deployment.keys.buildkite-agent-key = {
           text = secrets.buildkite-agent-key;
-          user = "buildkite-agent";
+          user = "buildkite-agent-ci";
           permissions = "0600";
         };
 
         deployment.keys."buildkite-agent-key.pub" = {
           text = secrets."buildkite-agent-key.pub";
-          user = "buildkite-agent";
+          user = "buildkite-agent-ci";
           permissions = "0600";
         };
 
@@ -81,7 +81,7 @@ in
 
         deployment.keys.github-nixpkgs-swh-key = {
           text = secrets.github-nixpkgs-swh-key;
-          user = "buildkite-agent";
+          user = "buildkite-agent-ci";
           permissions = "0400";
         };
 
