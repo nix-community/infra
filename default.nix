459d67eb6d4ba9387d8f4601f069555c8adc6b62
zimbatm <zimbatm@zimbatm.com>
2020-04-18 00:12:32 +0200

    fix ci?

---
+++ default.nix
@@ -0,0 +1,14 @@
+# Add derivations to be built from the cache to this file
+{ system ? builtins.currentSystem }:
+let
+  pkgs = import ./nix { inherit system; };
+
+  importNixOS = configuration: system:
+    (import "${toString pkgs.path}/nixos") {
+      inherit configuration system;
+    };
+in
+pkgs.nix-community-infra // rec {
+  build01 = importNixOS ./build01/configuration.nix "x86_64-linux";
+  build01-system = build01.system;
+}
