7eb50cb8270a18ff77360c2748c23d877f182e2e
Ryan Mulligan <ryan@ryantm.com>
2020-06-13 20:23:50 -0700

    update nixpkgs-update
    - fix delete-done

--- build01/nixpkgs-update.nix
+++ build01/nixpkgs-update.nix
@@ -55,7 +55,7 @@ in
 
   systemd.services.nixpkgs-update = {
     description = "nixpkgs-update service";
-    enable = false;
+    enable = true;
     restartIfChanged = false;
     path = nixpkgsUpdateSystemDependencies;
     environment.XDG_CONFIG_HOME = "/var/lib/nixpkgs-update";
@@ -84,7 +84,7 @@ in
 
   systemd.timers.nixpkgs-update = {
     description = "nixpkgs-update";
-    enable = false;
+    enable = true;
     timerConfig = { OnCalendar = "daily"; };
   };
 
