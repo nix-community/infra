96b735946cb73bb75c7d738234f980e50c09d22f
zimbatm <zimbatm@zimbatm.com>
2020-08-08 15:40:04 +0200

    matterbridge: fix config access

--- build01/matterbridge.nix
+++ build01/matterbridge.nix
@@ -1,4 +1,6 @@
 { ... }: {
   services.matterbridge.enable = true;
   services.matterbridge.configPath = "/run/keys/matterbridge.toml";
+  # Allow to access /run/keys
+  users.users.matterbridge.extraGroups = ["keys"];
 }
