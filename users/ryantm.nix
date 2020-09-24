07efc7cb643a8302f64ae13a7eafb493e62314c9
Ryan Mulligan <ryan@ryantm.com>
2020-09-24 09:02:14 -0700

    update ryantm's ssh key

--- users/ryantm.nix
+++ users/ryantm.nix
@@ -3,7 +3,7 @@
 let
   userLib = import ./lib.nix { inherit lib; };
   keys = [
-    "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA5KESKmapziCEd05LPnW1Ib+t5N18aZ8nzeVSZ3w79vGZHacgwKrGAQkQ1JbEFsm1aXQ4LR27l7Y5MM+auf0YZdGjtAiSsV/G/mjBP95HsuFTE1NSsXisdyKBkJ1g8TUfNOq2gsFyUVCeLMz4fC/ZYxdfBRpPnA6lCblWPmwLAaKTuI7afLv9UGN36/lFKReFzLpMfjYu/HAOYglRuQr8UcYvuysfDKwHImZYdZbzId2pg52nntSAiRgavjt2StiXVQz8zrCtvkguAkG6R8ZSPDyIJ0gLPNLxryIVLPscRbmH0usr3ipemOEplIsiNwp9pW2AQj0jZMBA55T75jxW2Q== ryantm-personal"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO37rmfhCI8e1aflMe1AlfK3zf9tXPHjV9dCb1FBupWt"
   ];
 
 in
