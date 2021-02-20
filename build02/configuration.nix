a67efcfe20bc1ac5dce2256d4381f0e8dcc2f953
Jörg Thalheim <joerg@thalheim.io>
2021-02-20 17:13:54 +0100

    build02: enable ipv6

--- build02/configuration.nix
+++ build02/configuration.nix
@@ -31,10 +31,17 @@ in
   networking.hostId = "af9ccc71";
   networking.useDHCP = false;
   networking.defaultGateway = "95.217.109.129";
+  networking.defaultGateway6 = {
+    address = "fe80::1";
+    interface = "enp35s0";
+  };
   networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
   networking.interfaces."enp35s0" = {
     ipv4.addresses = [ { address = "95.217.109.189"; prefixLength = 26; } ];
-    ipv6.addresses = [ { address = "fe80::aaa1:59ff:fe0e:aa61"; prefixLength = 64; } ];
+    ipv6.addresses = [
+      { address = "fe80::aaa1:59ff:fe0e:aa61"; prefixLength = 64; }
+      { address = "2a01:4f9:4a:2b02::1"; prefixLength = 64; }
+    ];
   };
 
   # nginx is being used as the frontend HTTP server for all the services
