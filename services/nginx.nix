095585c51578c3742464929853198bf04da26fee
Jörg Thalheim <joerg@thalheim.io>
2021-03-04 11:05:17 +0100

    nginx: enable recommend options

--- services/nginx.nix
+++ services/nginx.nix
@@ -1,9 +1,33 @@
 { config, lib, pkgs, ... }:
 
 {
+  networking.firewall.allowedTCPPorts = [ 443 80 ];
+
   # nginx is being used as the frontend HTTP server for all the services
   # running on the box
-  services.nginx.enable = true;
+  services.nginx = {
+    enable = true;
 
-  networking.firewall.allowedTCPPorts = [ 443 80 ];
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+
+    # Should we have this?
+    #commonHttpConfig = ''
+    #  add_header Strict-Transport-Security 'max-age=31536000; includeSubDomains; preload' always;
+    #'';
+
+    resolver.addresses =
+      if config.networking.nameservers == [ ]
+      then [ "1.1.1.1" ]
+      else config.networking.nameservers;
+
+    sslDhparam = config.security.dhparams.params.nginx.path;
+  };
+
+  security.dhparams = {
+    enable = true;
+    params.nginx = { };
+  };
 }
