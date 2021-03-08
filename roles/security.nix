94569f1ec3be93c2e568de7cb0d9a60fac8e731d
zimbatm <zimbatm@zimbatm.com>
2021-03-08 21:05:18 +0100

    fixup! roles/security: allow sudo without password (#63)
    pam sudo is not needed by virtual of the passwordless sudo

--- roles/security.nix
+++ roles/security.nix
@@ -4,12 +4,6 @@
   # Make sure that the firewall is enabled, even if it's the default.
   networking.firewall.enable = true;
 
-  # Allow sudo from SSH authenticated users
-  # This requires users in the wheel group to log in
-  # over ssh with an agent and enable forwarding
-  security.pam.services.sudo.sshAgentAuth = true;
-  security.pam.enableSSHAgentAuth = true;
-
   # Allow password-less sudo for wheel users
   security.sudo.enable = true;
   security.sudo.wheelNeedsPassword = false;
