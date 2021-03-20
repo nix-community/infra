c5870a9e8c046391612a17e7fb41bf9da6e1ce6e
Jörg Thalheim <joerg@thalheim.io>
2021-03-20 06:20:40 +0100

    build02: switch to hetzner networking module

--- build02/configuration.nix
+++ build02/configuration.nix
@@ -7,6 +7,7 @@
     ./nixpkgs-update.nix
 
     ../roles/common.nix
+    ../roles/hetzner-network.nix
     ../roles/nginx.nix
     ../roles/nix-community-cache.nix
   ];
@@ -19,18 +20,12 @@
   networking.hostName = "nix-community-build02";
   networking.hostId = "af9ccc71";
   networking.useDHCP = false;
-  networking.defaultGateway = "95.217.109.129";
-  networking.defaultGateway6 = {
-    address = "fe80::1";
-    interface = "enp35s0";
-  };
   networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
-  networking.interfaces."enp35s0" = {
-    ipv4.addresses = [{ address = "95.217.109.189"; prefixLength = 26; }];
-    ipv6.addresses = [
-      { address = "fe80::aaa1:59ff:fe0e:aa61"; prefixLength = 64; }
-      { address = "2a01:4f9:4a:2b02::1"; prefixLength = 64; }
-    ];
+
+  networking.nix-community = {
+    ipv4.address = "95.217.109.189";
+    ipv4.gateway = "95.217.109.129";
+    ipv6.address = "2a01:4f9:4a:2b02::1";
   };
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
