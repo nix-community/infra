c8ab60c2098cda8506175b7a8afa4a9a5abdfcb9
Jörg Thalheim <joerg@thalheim.io>
2021-06-08 11:10:16 +0200

    fix eval

--- roles/zfs.nix
+++ roles/zfs.nix
@@ -4,7 +4,7 @@
     # defaults to 12, which is a bit much given how much data is written
     autoSnapshot.monthly = 1;
     autoScrub.enable = true;
-    # zfs is not available for latest lts
-    enableUnstable = true;
   };
+  # zfs is not available for latest lts kenrel
+  boot.zfs.enableUnstable = true;
 }
