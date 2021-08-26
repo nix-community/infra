2ea7b938556c3a8df77fc86819417495d403f57c
Jörg Thalheim <joerg@thalheim.io>
2021-08-26 23:24:44 +0200

    build01: install some packages for nixpkgs development

---
+++ roles/dev-packages.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }:
+{
+  # useful for people that want to test stuff
+  environment.systemPackages = [
+    pkgs.vim
+    pkgs.nano # ?
+    pkgs.tmux
+    pkgs.git
+    pkgs.tig
+    pkgs.nixpkgs-review
+    pkgs.ripgrep
+    pkgs.fd
+  ];
+}
