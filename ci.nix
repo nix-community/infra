f9cc2ccbc9d011fbe05337f114af7b7e584b1de2
Jörg Thalheim <joerg@thalheim.io>
2021-12-23 21:15:46 +0100

    add continous integration

--- ci.nix
+++ ci.nix
@@ -1,5 +1,7 @@
 # Add derivations to be built from the cache to this file
-{ system ? builtins.currentSystem }:
+{ system ? builtins.currentSystem
+, src ? { ref = null; }
+}:
 let
   pkgs = import ./nix { inherit system; };
 
@@ -17,4 +19,12 @@ pkgs.nix-community-infra // rec {
   build03-system = build03.system;
   build04 = importNixOS ./build04/configuration.nix "aarch64-linux";
   build04-system = build04.system;
+  deploy-all = pkgs.effects.runIf (src.ref == "refs/heads/master") (pkgs.effects.runCachixDeploy {
+    deploy.agents = {
+      "nix-community-build01" = build01.config.system.build.toplevel;
+      "nix-community-build02" = build02.config.system.build.toplevel;
+      "nix-community-build03" = build03.config.system.build.toplevel;
+      "nix-community-build04" = build04.config.system.build.toplevel;
+    };
+  });
 }
