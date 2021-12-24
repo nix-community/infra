f2707303ba9c4aa77048c89df001dc9f4acca284
Jörg Thalheim <joerg@thalheim.io>
2021-12-24 11:03:08 +0100

    cachix-deploy: add nix to the path

--- roles/cachix-deploy/default.nix
+++ roles/cachix-deploy/default.nix
@@ -5,6 +5,7 @@
     sources = import ../../nix/sources.nix {};
   in {
     wantedBy = [ "multi-user.target" ];
+    path = [ config.nix.package ];
     serviceConfig = {
       EnvironmentFile = config.sops.secrets.cachix-agent-token.path;
       ExecStart = "${import sources.cachix {}}/bin/cachix deploy agent ${config.networking.hostName}";
