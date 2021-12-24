391d1a83d34d5c25e8758020cb17039c414f5d9b
Jörg Thalheim <joerg@thalheim.io>
2021-12-24 11:01:09 +0100

    fix cachix deploy agent

--- roles/cachix-deploy/default.nix
+++ roles/cachix-deploy/default.nix
@@ -1,11 +1,13 @@
 { config, pkgs, ... }: {
   sops.secrets.cachix-agent-token.sopsFile = ./secrets.yaml;
 
-  systemd.services.cachix-deploy-agent = {
+  systemd.services.cachix-deploy-agent = let
+    sources = import ../../nix/sources.nix {};
+  in {
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
       EnvironmentFile = config.sops.secrets.cachix-agent-token.path;
-      ExecStart = "${pkgs.cachix}/bin/cachix deploy agent ${config.networking.hostName}";
+      ExecStart = "${import sources.cachix {}}/bin/cachix deploy agent ${config.networking.hostName}";
     };
   };
 }
