1e9f69c27811ed9914a2c0223915b7f8a6b256d8
Ryan Mulligan <ryan@ryantm.com>
2021-12-27 21:38:09 -0800

    build02: remove test-cachix service

--- build02/nixpkgs-update.nix
+++ build02/nixpkgs-update.nix
@@ -99,15 +99,6 @@ in
     '';
   };
 
-  systemd.services.nixpkgs-update-test-cachix = mkNixpkgsUpdateService "test-cachix" // {
-    script = ''
-      echo $HOME
-      ls -la $HOME/.config/cachix
-      cachix push nix-community /nix/store/24j9hfzwddscpzz3027hcd1rzm0ar1v9-xh-0.14.1
-    '';
-  };
-
-
   programs.ssh.knownHosts.github-rsa = {
     hostNames = [ "github.com" ];
     publicKey = "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==";
