98fc65decb9b40d145635594ed6b1f7aeb7de0f5
Jörg Thalheim <joerg@thalheim.io>
2022-01-15 08:13:13 +0100

    speed up deployment process a bit

--- ci.nix
+++ ci.nix
@@ -9,22 +9,27 @@ let
     (import "${toString pkgs.path}/nixos") {
       inherit configuration system;
     };
+
+  deploy = machine: pkgs.effects.runIf (src.ref == "refs/heads/master") (pkgs.effects.runCachixDeploy {
+    deploy.agents = {
+      ${machine.config.networking.hostName} = machine.config.system.build.toplevel;
+    };
+  });
 in
 pkgs.nix-community-infra // rec {
   build01 = importNixOS ./build01/configuration.nix "x86_64-linux";
   build01-system = build01.system;
+  build01-deploy = deploy build01;
+
   build02 = importNixOS ./build02/configuration.nix "x86_64-linux";
   build02-system = build02.system;
+  build02-deploy = deploy build02;
+
   build03 = importNixOS ./build03/configuration.nix "x86_64-linux";
   build03-system = build03.system;
+  build03-deploy = deploy build03;
+
   build04 = importNixOS ./build04/configuration.nix "aarch64-linux";
   build04-system = build04.system;
-  deploy-all = pkgs.effects.runIf (src.ref == "refs/heads/master") (pkgs.effects.runCachixDeploy {
-    deploy.agents = {
-      "nix-community-build01" = build01.config.system.build.toplevel;
-      "nix-community-build02" = build02.config.system.build.toplevel;
-      "nix-community-build03" = build03.config.system.build.toplevel;
-      "nix-community-build04" = build04.config.system.build.toplevel;
-    };
-  });
+  build04-deploy = deploy build04;
 }
