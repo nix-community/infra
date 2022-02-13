3469fba031a8e72b20cd188167e14e98467f07c2
Jörg Thalheim <joerg@thalheim.io>
2022-02-13 13:55:54 +0100

    cachix-deploy: add restart service

--- roles/cachix-deploy/default.nix
+++ roles/cachix-deploy/default.nix
@@ -1,6 +1,22 @@
 { config, pkgs, ... }: {
   sops.secrets.cachix-agent-token.sopsFile = ./secrets.yaml;
 
+  # We don't restart cachix-deploy-agent on upgrades.
+  # So we do it at random points in time instead.
+  # Also helps with connection failures.
+  systemd.services.cachix-deploy-restart = {
+    wantedBy = [ "multi-user.target" ];
+    startAt = "hourly";
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = [
+        # sleep in case we are restarted by an upgrade process.
+        "${pkgs.coreutils}/bin/sleep 600"
+        "${config.systemd.package}/bin/systemctl restart cachix-deploy-agent"
+      ];
+    };
+  };
+
   systemd.services.cachix-deploy-agent = {
     wantedBy = [ "multi-user.target" ];
     path = [ config.nix.package ];
