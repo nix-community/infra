e348437df444bbd250c3121885922923c2ed69e3
Jörg Thalheim <joerg@thalheim.io>
2022-04-11 09:59:06 +0200

    zfs: unset disk scheduler for zfs

--- roles/zfs.nix
+++ roles/zfs.nix
@@ -7,4 +7,9 @@
   };
   # zfs is not available for latest lts kenrel
   boot.zfs.enableUnstable = true;
+
+  # ZFS already has its own scheduler. Without this my(@Artturin) computer froze for a second when i nix build something.
+  services.udev.extraRules = ''
+    ACTION=="add|change", KERNEL=="sd[a-z]*[0-9]*|mmcblk[0-9]*p[0-9]*|nvme[0-9]*n[0-9]*p[0-9]*", ENV{ID_FS_TYPE}=="zfs_member", ATTR{../queue/scheduler}="none"
+  '';
 }
