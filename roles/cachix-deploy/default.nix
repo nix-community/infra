7f0fe664465f696be041bb5902ec41eb17242b22
Jörg Thalheim <joerg@thalheim.io>
2022-07-14 06:51:28 +0200

    cachix-deploy: switch to upstream service

--- roles/cachix-deploy/default.nix
+++ roles/cachix-deploy/default.nix
@@ -1,31 +1,9 @@
 { config, pkgs, ... }: {
   sops.secrets.cachix-agent-token.sopsFile = ./secrets.yaml;
 
-  # We don't restart cachix-deploy-agent on upgrades.
-  # So we do it at random points in time instead.
-  # Also helps with connection failures.
-  systemd.services.cachix-deploy-restart = {
-    wantedBy = [ "multi-user.target" ];
-    startAt = "hourly";
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = [
-        # sleep in case we are restarted by an upgrade process.
-        "${pkgs.coreutils}/bin/sleep 600"
-        "${config.systemd.package}/bin/systemctl restart cachix-deploy-agent"
-      ];
-    };
-  };
-
-  systemd.services.cachix-deploy-agent = {
-    wantedBy = [ "multi-user.target" ];
-    path = [ config.nix.package ];
-    restartIfChanged = false;
-    serviceConfig = {
-      Restart = "on-failure";
-      Environment = "USER=root";
-      EnvironmentFile = config.sops.secrets.cachix-agent-token.path;
-      ExecStart = "${pkgs.cachix}/bin/cachix deploy agent ${config.networking.hostName}";
-    };
+  services.cachix-agent = {
+    enable = true;
+    credentialsFile = config.sops.secrets.cachix-agent-token.path;
+    verbose = true;
   };
 }
