01383614bc6aea6e92e755359e1efd8bb4e3f266
Jörg Thalheim <joerg@thalheim.io>
2022-08-13 18:05:37 +0200

    build04: remote builder needs to be a normal user

--- roles/builder.nix
+++ roles/builder.nix
@@ -1,8 +1,9 @@
 {
   users.users.nix = {
-    isSystemUser = true;
+    isNormalUser = true;
     group = "nix";
     home = "/var/lib/nix";
+    createHome = true;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEmdo1x1QkRepZf7nSe+OdEWX+wOjkBLF70vX9F+xf68 builder"
     ];
