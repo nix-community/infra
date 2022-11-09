19fcaa7647a5707dda43920bbc7a6a12211d7024
Jörg Thalheim <joerg@thalheim.io>
2022-11-09 09:06:54 +0100

    swap: fix fsname

--- roles/swap.nix
+++ roles/swap.nix
@@ -1,5 +1,5 @@
 # https://github.com/zfsonlinux/pkg-zfs/wiki/HOWTO-use-a-zvol-as-a-swap-device
-# zfs create -V 16G -b $(getconf PAGESIZE) -o logbias=throughput -o sync=always -o primarycache=metadata -o com.sun:auto-snapshot=false /dev/zvol/zroot/swap
+# zfs create -V 16G -b $(getconf PAGESIZE) -o logbias=throughput -o sync=always -o primarycache=metadata -o com.sun:auto-snapshot=false zroot/swap
 # mkswap -f /dev/zvol/zroot/swap
 {
   swapDevices = [{ device = "/dev/zvol/zroot/swap"; }];
