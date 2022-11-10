b8fee237403e1133a153997cb94c060b1869e81b
Jörg Thalheim <joerg@thalheim.io>
2022-11-10 14:42:41 +0100

    build02: decrease number of worker

--- build02/nixpkgs-update.nix
+++ build02/nixpkgs-update.nix
@@ -148,8 +148,9 @@ in
 
   systemd.services.nixpkgs-update-worker1 = mkWorker "worker1";
   systemd.services.nixpkgs-update-worker2 = mkWorker "worker2";
-  systemd.services.nixpkgs-update-worker3 = mkWorker "worker3";
-  systemd.services.nixpkgs-update-worker4 = mkWorker "worker4";
+  # Too many workers cause out-of-memory.
+  #systemd.services.nixpkgs-update-worker3 = mkWorker "worker3";
+  #systemd.services.nixpkgs-update-worker4 = mkWorker "worker4";
 
   systemd.tmpfiles.rules = [
     "L+ /home/r-ryantm/.gitconfig - - - - ${./gitconfig.txt}"
