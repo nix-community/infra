ebeccfa9ab4b155eb08a395430f25ea28702396f
zowoq <59103226+zowoq@users.noreply.github.com>
2022-11-28 17:06:04 +1000

    autoUpgrade: add accept-flake-config flag
    silence "untrusted flake configuration" warning

--- roles/auto-upgrade.nix
+++ roles/auto-upgrade.nix
@@ -3,4 +3,5 @@
   system.autoUpgrade.flake = "github:nix-community/infra";
   system.autoUpgrade.allowReboot = true;
   system.autoUpgrade.dates = "hourly";
+  system.autoUpgrade.flags = [ "--option" "accept-flake-config" "true" ];
 }
