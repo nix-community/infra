b5d2bfc3a27e4a21d39754520bbcb22b8ff93c83
zimbatm <zimbatm@zimbatm.com>
2023-01-22 12:17:49 +0100

    terraform: invite mic92 to terraform cloud

--- terraform/terraform_cloud.tf
+++ terraform/terraform_cloud.tf
@@ -6,7 +6,9 @@ locals {
   # FIXME: add all the admins of the org
   # NOTE: there is a limit of 5 members in the free plan
   tfe_owners = {
-    zimbatm = "zimbatm@zimbatm.com"
+    mic92 = "joerg@thalheim.io"
+    # FIXME: I need to find the user ID for my account before this can be used
+    # zimbatm = "zimbatm@zimbatm.com"
   }
 
   tfe_org = "nix-community" #tfe_organization.nix-community.name
@@ -27,18 +29,17 @@ resource "tfe_team" "owners" {
   organization = "nix-community"
 }
 
-# FIXME: I need to find the user ID for my account before this can be used
-# resource "tfe_organization_membership" "owners" {
-#   for_each     = local.tfe_owners
-#   organization = local.tfe_org
-#   email        = each.value
-# }
+resource "tfe_organization_membership" "owners" {
+  for_each     = local.tfe_owners
+  organization = local.tfe_org
+  email        = each.value
+}
 
-# resource "tfe_team_organization_member" "owners" {
-#   for_each                   = local.tfe_owners
-#   team_id                    = tfe_team.owners.id
-#   organization_membership_id = tfe_organization_membership.owners[each.key].id
-# }
+resource "tfe_team_organization_member" "owners" {
+  for_each                   = local.tfe_owners
+  team_id                    = tfe_team.owners.id
+  organization_membership_id = tfe_organization_membership.owners[each.key].id
+}
 
 # Workspaces setup
 
