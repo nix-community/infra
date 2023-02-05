fad4356a85f8a2b34c5eded0ddd082c04d0d815a
zowoq <59103226+zowoq@users.noreply.github.com>
2023-02-05 13:30:10 +1000

    roles/hercules-ci: removed outdated secrets

--- roles/hercules-ci/default.nix
+++ roles/hercules-ci/default.nix
@@ -9,13 +9,11 @@ in
 {
   sops.secrets."binary-caches.json" = herculesSecret;
   sops.secrets."cluster-join-token.key" = herculesSecret;
-  sops.secrets."hercules-secrets" = herculesSecret;
 
   services.hercules-ci-agent = {
     enable = true;
     settings = {
       binaryCachesPath = secrets."binary-caches.json".path;
-      secretsJsonPath = secrets."hercules-secrets".path;
       clusterJoinTokenPath = secrets."cluster-join-token.key".path;
     };
   };
