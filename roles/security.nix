81dd4e0557c2a5a9e59a3b1546f098044f18b61b
zowoq <59103226+zowoq@users.noreply.github.com>
2023-04-25 10:21:07 +1000

    roles/security: only allow ssh_host_ed25519_key

--- roles/security.nix
+++ roles/security.nix
@@ -25,6 +25,12 @@
     };
   };
 
+  services.openssh = {
+    hostKeys = [
+      { path = "/etc/ssh/ssh_host_ed25519_key"; type = "ed25519"; }
+    ];
+  };
+
   # Ban brute force SSH
   services.fail2ban.enable = true;
 }
