9787a9860dbbbda4638efb81b896459059897f69
zowoq <59103226+zowoq@users.noreply.github.com>
2023-05-07 11:07:50 +1000

    roles/watch-store: unpin cachix

--- roles/watch-store.nix
+++ roles/watch-store.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, ... }:
 
 {
   sops.secrets.watch-store-token.sopsFile = ./nix-community-cache/secrets.yaml;
@@ -7,6 +7,5 @@
     enable = true;
     cacheName = "nix-community";
     cachixTokenFile = config.sops.secrets.watch-store-token.path;
-    package = pkgs.haskellPackages.cachix_1_3_3;
   };
 }
