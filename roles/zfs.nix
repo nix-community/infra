22249eda0cab64d221d052430cd78a94670dc112
zowoq <59103226+zowoq@users.noreply.github.com>
2023-05-10 12:43:41 +1000

    roles/zfs-raid: split into raid and zfs

--- roles/zfs.nix
+++ roles/zfs.nix
@@ -1,8 +1,4 @@
 {
-  # for mdraid 1.1
-  boot.loader.grub.extraConfig = "insmod mdraid1x";
-  services.telegraf.extraConfig.inputs.mdstat = { };
-
   fileSystems."/" = {
     device = "zroot/root/nixos";
     fsType = "zfs";
