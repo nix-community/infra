bad392dadbd2b3f4be60db050c5805337cbb3718
zowoq <59103226+zowoq@users.noreply.github.com>
2023-05-24 16:02:18 +1000

    effect: refactor

--- effect.nix
+++ effect.nix
@@ -1,31 +1,28 @@
 { withSystem, ... }:
 {
   herculesCI = { config, ... }:
-    let
-      inherit (config.repo) ref;
-    in
-    {
-      onPush.default.outputs.effects = withSystem "x86_64-linux" ({ hci-effects, pkgs, self', ... }:
-        {
-          terraform-deploy =
-            hci-effects.runIf (pkgs.lib.hasPrefix "refs/heads/gh-readonly-queue/master/" ref)
-              (hci-effects.mkEffect {
-                name = "terraform-deploy";
-                inputs = [ self'.devShells.terraform.nativeBuildInputs ];
-                src = pkgs.lib.cleanSource ./.;
-                secretsMap.tf-secrets = "tf-secrets";
-                effectScript = ''
-                  export TF_IN_AUTOMATION=1
-                  export TF_INPUT=0
-                  export SOPS_AGE_KEY="$(readSecretString tf-secrets .SOPS_AGE_KEY)"
-                  export TF_TOKEN_app_terraform_io="$(readSecretString tf-secrets .TF_TOKEN_app_terraform_io)"
+    withSystem "x86_64-linux" ({ hci-effects, pkgs, self', ... }:
+      {
+        onPush.default.outputs.effects = {
+          terraform-deploy = hci-effects.runIf (pkgs.lib.hasPrefix "refs/heads/gh-readonly-queue/master/" config.repo.ref)
+            (hci-effects.mkEffect {
+              name = "terraform-deploy";
+              inputs = [ self'.devShells.terraform.nativeBuildInputs ];
+              src = pkgs.lib.cleanSource ./.;
+              secretsMap.tf-secrets = "tf-secrets";
+              effectScript = ''
+                export TF_IN_AUTOMATION=1
+                export TF_INPUT=0
+                export SOPS_AGE_KEY="$(readSecretString tf-secrets .SOPS_AGE_KEY)"
+                export TF_TOKEN_app_terraform_io="$(readSecretString tf-secrets .TF_TOKEN_app_terraform_io)"
 
-                  pushd terraform
-                  terraform init
-                  terraform validate
-                  terraform apply -auto-approve
-                '';
-              });
-        });
-    };
+                pushd terraform
+                terraform init
+                terraform validate
+                terraform apply -auto-approve
+              '';
+            });
+        };
+      }
+    );
 }
