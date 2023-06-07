83ca767304bbfe3b7e8dd4107f7b0e1923274ce2
zowoq <59103226+zowoq@users.noreply.github.com>
2023-06-08 08:18:39 +1000

    hosts/darwin02: use unstable telegraf

--- hosts/darwin02/telegraf.nix
+++ hosts/darwin02/telegraf.nix
@@ -1,6 +1,22 @@
+{ pkgs, ... }:
 {
   services.telegraf = {
     enable = true;
+    # can switch back to nixpkgs after > 1.26.3
+    package = pkgs.buildGoModule rec {
+      pname = "telegraf";
+      version = "1.26.3";
+      subPackages = [ "cmd/telegraf" ];
+      src = pkgs.fetchFromGitHub {
+        owner = "influxdata";
+        repo = "telegraf";
+        rev = "ebe346103ebfd96b63cdbd2f3f36b2258746c160";
+        hash = "sha256-j6VWbF6B7+TQlLNdQMwN7r4kOUGQi5c/PigNZhJA2Zk=";
+      };
+      vendorHash = "sha256-4WmXEa42zyeRGcdXe4REILYNMZZ83FVzDDaKFMnx37Y=";
+      proxyVendor = true;
+      ldflags = [ "-s" "-w" "-X=github.com/influxdata/telegraf/internal.Commit=${src.rev}" "-X=github.com/influxdata/telegraf/internal.Version=${version}" ];
+    };
     extraConfig = {
       agent.interval = "60s";
       inputs = {
