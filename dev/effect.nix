6d261cac5910f042edd892b47cabbcd543933e6a
zowoq <59103226+zowoq@users.noreply.github.com>
2023-06-10 21:10:16 +1000

    dev/effect: avoid downloading closure on the deploying machine
    copied from 428fa510894873cc5ce7bc1e8008224bc0d759cf

--- dev/effect.nix
+++ dev/effect.nix
@@ -2,18 +2,26 @@
 {
   herculesCI = { config, ... }:
     withSystem "x86_64-linux" ({ hci-effects, pkgs, self', ... }:
+      let
+        # using the drv path here avoids downloading the closure on the deploying machine
+        drv = builtins.unsafeDiscardStringContext self.darwinConfigurations.darwin02.config.system.build.toplevel.drvPath;
+      in
       {
         onPush.default.outputs.effects = {
           darwin-deploy = hci-effects.runIf (pkgs.lib.hasPrefix "refs/heads/gh-readonly-queue/master/" config.repo.ref)
-            (hci-effects.runNixDarwin {
-              config = self.darwinConfigurations.darwin02;
+            (hci-effects.mkEffect {
               secretsMap.hercules-ssh = "hercules-ssh";
-              ssh.destination = "m1@darwin02.nix-community.org";
-              userSetupScript = ''
+              effectScript = ''
                 writeSSHKey hercules-ssh
                 cat >>~/.ssh/known_hosts <<EOF
                 darwin02.nix-community.org ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBt6uTauhRbs5A6jwAT3p3i3P1keNC6RpaA1Na859BCa
                 EOF
+                ${hci-effects.ssh { destination = "m1@darwin02.nix-community.org"; } ''
+                  set -eu
+                  newProfile=$(nix-store --realise ${drv})
+                  sudo -H nix-env --profile /nix/var/nix/profiles/system --set $newProfile
+                  $newProfile/sw/bin/darwin-rebuild activate
+                ''}
               '';
             });
           terraform-deploy = hci-effects.runIf (pkgs.lib.hasPrefix "refs/heads/gh-readonly-queue/master/" config.repo.ref)
