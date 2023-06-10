ff5253a7cf15d6a1b7e9bb54a798ccea805934d6
zowoq <59103226+zowoq@users.noreply.github.com>
2023-06-11 08:29:02 +1000

    modules/remote-builder: aarch64-build04 -> build04

--- flake.nix
+++ flake.nix
@@ -133,8 +133,8 @@
           hydra = ./modules/nixos/hydra;
           nur-update = ./modules/nixos/nur-update;
           raid = ./modules/nixos/raid.nix;
-          remote-builder-aarch64-build04 = ./modules/nixos/remote-builder/aarch64-build04.nix;
           remote-builder-aarch64-nixos-community = ./modules/nixos/remote-builder/aarch64-nixos-community.nix;
+          remote-builder-build04 = ./modules/nixos/remote-builder/build04.nix;
           remote-builder-darwin02 = ./modules/nixos/remote-builder/darwin02.nix;
           remote-builder-user = ./modules/nixos/remote-builder/user.nix;
           watch-store = ./modules/nixos/watch-store.nix;
