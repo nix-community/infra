784d428a0d3d33bdfa77c6a8d153ce27e97f4c7c
adisbladis <adisbladis@gmail.com>
2023-07-05 22:19:19 +1200

    web01: Add SMB mounts for Hetzner Storage Box

--- hosts/web01/configuration.nix
+++ hosts/web01/configuration.nix
@@ -6,6 +6,7 @@
     inputs.srvos.nixosModules.mixins-nginx
     inputs.srvos.nixosModules.hardware-hetzner-cloud
     inputs.self.nixosModules.common
+    ./samba.nix
   ];
 
   networking.hostName = "web01";
---
+++ hosts/web01/samba.nix
@@ -0,0 +1,50 @@
+{ config, pkgs, ... }:
+
+let
+  inherit (builtins) concatStringsSep;
+
+  hostname = "u348918.your-storagebox.de";
+
+in
+{
+  sops.secrets.hetzner-cifs-lemmy-pict-rs-creds = { };
+  sops.secrets.hetzner-cifs-web01-pgbackrest = { };
+
+  fileSystems =
+    let
+      cifsOpts = [
+        "x-systemd.automount"
+        "noauto"
+        "x-systemd.idle-timeout=60"
+        "x-systemd.device-timeout=5s"
+        "x-systemd.mount-timeout=5s"
+      ];
+    in
+    {
+
+      "/mnt/lemmy-pict-rs" = {
+        device = "//${hostname}/u348918-sub1";
+        fsType = "cifs";
+        options = [
+          (concatStringsSep "," (cifsOpts ++ [
+            "credentials=${config.sops.secrets.hetzner-cifs-lemmy-pict-rs-creds.path}"
+          ]))
+        ];
+      };
+
+      "/mnt/pgbackrest" = {
+        device = "//${hostname}/u348918-sub2";
+        fsType = "cifs";
+        options = [
+          (concatStringsSep "," (cifsOpts ++ [
+            "uid=${toString config.users.users.postgres.uid}"
+            "gid=${toString config.users.groups.postgres.gid}"
+            "credentials=${config.sops.secrets.hetzner-cifs-web01-pgbackrest.path}"
+          ]))
+        ];
+      };
+
+    };
+
+  environment.systemPackages = [ pkgs.cifs-utils ];
+}
