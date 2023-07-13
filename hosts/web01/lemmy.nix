2a061ea3e168004ca734cd10be240e32764bde64
adisbladis <adisbladis@gmail.com>
2023-07-13 16:06:00 +1200

    web01: Configure pict-rs

--- hosts/web01/lemmy.nix
+++ hosts/web01/lemmy.nix
@@ -1,16 +1,23 @@
-{ inputs, pkgs, lib, ... }:
+{ inputs, pkgs, lib, config, ... }:
 
 let
   domain = "lemmy.nix-community.org";
 in
 {
+  sops.secrets.pictrs-env = { };
+  sops.secrets.lemmy-secretfile = { };
+
   services.lemmy = {
     enable = true;
     nginx.enable = true;
     database.createLocally = true;
+    secretFile = config.sops.secrets.lemmy-secretfile.path;
 
     settings = {
       hostname = domain;
+      pictrs = {
+        url = with config.services.pict-rs; "http://${address}:${toString port}";
+      };
     };
   };
 
@@ -30,7 +37,11 @@ in
   # 0.4 is still not released but we need this separation already.
   # This renames the env vars set by the NixOS module.
   #
-  # Migrate to using upstream NixOS definitions when 0.4 is released.
+  # Migrate to using upstream NixOS definitions when 0.4 is in nixpkgs.
+  systemd.services.pict-rs.serviceConfig.EnvironmentFile = [
+    config.sops.secrets.pictrs-env.path
+  ];
+  systemd.services.pict-rs.unitConfig.RequiresMountsFor = [ "/mnt/lemmy-pict-rs" ];
   systemd.services.pict-rs.environment = {
     PICTRS__REPO__PATH = "/var/lib/pict-rs/sled";
     PICTRS__STORE__PATH = "/mnt/lemmy-pict-rs";
