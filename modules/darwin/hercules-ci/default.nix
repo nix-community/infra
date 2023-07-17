2a04588d9a5746c6a4918eb9b73faa9bab922565
zowoq <59103226+zowoq@users.noreply.github.com>
2023-07-17 13:53:06 +1000

    modules/darwin: refactor

---
+++ modules/darwin/hercules-ci/default.nix
@@ -0,0 +1,5 @@
+{
+  # hercules secrets are installed manually from ./secrets.yaml
+  # https://docs.hercules-ci.com/hercules-ci/getting-started/deploy/nix-darwin
+  services.hercules-ci-agent.enable = true;
+}
