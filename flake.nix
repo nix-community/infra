14495e88d0d77eede934f3f7c15ec275096f8dbd
zowoq <59103226+zowoq@users.noreply.github.com>
2023-07-21 14:34:49 +1000

    Revert "disable darwin"

--- flake.nix
+++ flake.nix
@@ -46,8 +46,6 @@
       {
         systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
-        flake.herculesCI.ciSystems = [ "x86_64-linux" "aarch64-linux" ];
-
         imports = [
           inputs.treefmt-nix.flakeModule
         ];
