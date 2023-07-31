1a1955404fac08acd53c51d42df9451fc2ae4a70
zowoq <59103226+zowoq@users.noreply.github.com>
2023-07-31 12:17:10 +1000

    modules/darwin/common: refactor authorizedKeys

--- hosts/darwin02/configuration.nix
+++ hosts/darwin02/configuration.nix
@@ -16,13 +16,4 @@
   networking.hostName = "darwin02";
 
   system.stateVersion = 4;
-
-  # TODO: refactor this to share /users with nixos
-  # if user is removed the keys need to be removed manually from /etc/ssh/authorized_keys.d
-  users.users.hetzner.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOG/9rsFqC2tg+W5YZxthW5xhUJEfZ8ShqkRtVe+A6+u" # hercules-ssh-deploy
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKbBp2dH2X3dcU1zh+xW3ZsdYROKpJd3n13ssOP092qE" # mic92
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOuiDoBOxgyer8vGcfAIbE6TC4n4jo8lhG9l01iJ0bZz" # zimbatm
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFbCYwWByGE46XHH4Q0vZgQ5sOUgbH50M8KO2xhBC4m/" # zowoq
-  ];
 }
