977424e2d7739ab76dfdcfb1c3a31a35288e47fc
Jörg Thalheim <joerg@thalheim.io>
2023-08-05 08:01:56 +0200

    retry nixos upgrade on failure

--- modules/nixos/common/auto-upgrade.nix
+++ modules/nixos/common/auto-upgrade.nix
@@ -3,4 +3,11 @@
   system.autoUpgrade.flake = "github:nix-community/infra";
   system.autoUpgrade.dates = "hourly";
   system.autoUpgrade.flags = [ "--option" "accept-flake-config" "true" "--option" "tarball-ttl" "0" ];
+
+  # Restart the upgrade service if it fails twice in 5 seconds.
+  systemd.services.nixos-upgrade = {
+    unitConfig.StartLimitBurst = 2;
+    unitConfig.StartLimitIntervalSec = 5;
+    serviceConfig.Restart = "on-failure";
+  };
 }
