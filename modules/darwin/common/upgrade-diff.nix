158d2ad2db56f383742c7d59843d4b3e89093ad8
zowoq <59103226+zowoq@users.noreply.github.com>
2023-08-08 13:04:28 +1000

    modules/darwin/common: add upgrade-diff

---
+++ modules/darwin/common/upgrade-diff.nix
@@ -0,0 +1,13 @@
+{ config, ... }:
+{
+  # set $HOME to avoid 'warning: $HOME is not owned by you'
+  # https://github.com/NixOS/nix/issues/6834
+  # srvos
+  system.activationScripts.postActivation.text = ''
+    if [[ -e /run/current-system ]]; then
+      echo "--- diff to current-system"
+      HOME=/var/root ${config.nix.package}/bin/nix --extra-experimental-features nix-command store diff-closures /run/current-system "$systemConfig"
+      echo "---"
+    fi
+  '';
+}
