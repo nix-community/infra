36ace4f9769c98c7e4306dfb29023dd08bbc2056
zowoq <59103226+zowoq@users.noreply.github.com>
2023-08-10 15:49:57 +1000

    modules/darwin/common/upgrade-diff: postActivation -> preActivation

--- modules/darwin/common/upgrade-diff.nix
+++ modules/darwin/common/upgrade-diff.nix
@@ -3,7 +3,7 @@
   # set $HOME to avoid 'warning: $HOME is not owned by you'
   # https://github.com/NixOS/nix/issues/6834
   # srvos
-  system.activationScripts.postActivation.text = ''
+  system.activationScripts.preActivation.text = ''
     if [[ -e /run/current-system ]]; then
       echo "--- diff to current-system"
       HOME=/var/root ${config.nix.package}/bin/nix --extra-experimental-features nix-command store diff-closures /run/current-system "$systemConfig"
