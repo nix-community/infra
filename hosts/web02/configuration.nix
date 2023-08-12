149ef6151f83c9cc3068e09cb562069480f63e02
zowoq <59103226+zowoq@users.noreply.github.com>
2023-08-12 12:40:49 +1000

    web02: cachix deploy

--- hosts/web02/configuration.nix
+++ hosts/web02/configuration.nix
@@ -5,6 +5,7 @@
     inputs.self.nixosModules.common
     inputs.self.nixosModules.monitoring
     inputs.srvos.nixosModules.mixins-nginx
+    inputs.self.nixosModules.cachix-deploy
   ];
 
   networking.hostName = "web02";
