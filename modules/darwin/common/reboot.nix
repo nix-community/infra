90fb983b7d0d4c37e50133ff5b901cadb852cd59
zowoq <59103226+zowoq@users.noreply.github.com>
2023-08-13 12:55:48 +1000

    modules/darwin/common/reboot: command -> script

--- modules/darwin/common/reboot.nix
+++ modules/darwin/common/reboot.nix
@@ -1,7 +1,7 @@
 {
   # reboot every sunday between 00:00 and 06:00
   launchd.daemons.reboot = {
-    command = ''/sbin/shutdown -r "+$(( $RANDOM % ( 6 * 60 ) ))"'';
+    script = ''/sbin/shutdown -r "+$(( $RANDOM % ( 6 * 60 ) ))"'';
     serviceConfig.StartCalendarInterval = [
       {
         Hour = 0;
