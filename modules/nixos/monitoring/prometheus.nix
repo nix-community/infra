2ea3c74a621bb57b441a5f5609d94e20c7816409
zowoq <59103226+zowoq@users.noreply.github.com>
2023-08-14 20:41:52 +1000

    modules/nixos/monitoring: refactor hosts

--- modules/nixos/monitoring/prometheus.nix
+++ modules/nixos/monitoring/prometheus.nix
@@ -18,23 +18,16 @@
         job_name = "telegraf";
         scrape_interval = "60s";
         metrics_path = "/metrics";
-        static_configs = [
-          {
-            targets = map (host: "${host}:9273")
-              [
-                "build01.nix-community.org"
-                "build02.nix-community.org"
-                "build03.nix-community.org"
-                "build04.nix-community.org"
-                "darwin02.nix-community.org"
-                "darwin03.nix-community.org"
-                "web01.nix-community.org"
-                #"web02.nix-community.org"
-                "localhost"
-              ];
-            labels.org = "nix-community";
-          }
-        ];
+        static_configs =
+          let
+            hosts = import ./hosts.nix;
+          in
+          [
+            {
+              targets = map (host: "${host}:9273") hosts ++ [ "localhost:9273" ];
+              labels.org = "nix-community";
+            }
+          ];
       }
     ];
     alertmanagers = [
