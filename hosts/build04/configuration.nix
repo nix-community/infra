403970df0f6798c62902efe41464b87d7e99917e
Jörg Thalheim <joerg@thalheim.io>
2023-08-21 16:40:59 +0200

    build04: kexec argument is no longer needed
    we have aarch64 support in nixos-anywhere

--- hosts/build04/configuration.nix
+++ hosts/build04/configuration.nix
@@ -43,6 +43,5 @@
 ## `opc` is the username from the oracle image. Replace with root if we are booted into nixos.
 # nix run github:numtide/nixos-anywhere#nixos-anywhere -- \
 #   --debug \
-#   --kexec "$(nix build --print-out-paths github:nix-community/nixos-images#packages.aarch64-linux.kexec-installer-nixos-unstable)/nixos-kexec-installer-aarch64-linux.tar.gz" \
 #   --flake '.#build04' \
 #   opc@141.148.235.248
