e536452b5ac746a700cd41edc43a017decd8c0ee
zowoq <59103226+zowoq@users.noreply.github.com>
2023-08-24 08:59:02 +1000

    web01, web02: switch back to autoupgrade

--- hosts/web02/configuration.nix
+++ hosts/web02/configuration.nix
@@ -5,7 +5,6 @@
     inputs.self.nixosModules.common
     inputs.self.nixosModules.monitoring
     inputs.srvos.nixosModules.mixins-nginx
-    inputs.self.nixosModules.cachix-deploy
   ];
 
   networking.hostName = "web02";
