357d8bc162341c21479170e3b6e93b833a4a3902
zowoq <59103226+zowoq@users.noreply.github.com>
2023-09-06 09:24:25 +1000

    modules/nixos/builder: increase open file limit

--- modules/nixos/builder.nix
+++ modules/nixos/builder.nix
@@ -4,4 +4,9 @@
   ];
 
   nix.gc.dates = "hourly";
+
+  # Bump the open files limit so that non-root users can run NixOS VM tests
+  security.pam.loginLimits = [
+    { domain = "*"; item = "nofile"; type = "-"; value = "20480"; }
+  ];
 }
