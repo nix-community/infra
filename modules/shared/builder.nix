352152dbd73ebcd6568feae130d0791b66caf3eb
zowoq <59103226+zowoq@users.noreply.github.com>
2023-09-10 10:05:36 +1000

    modules/shared/builder: fix max-freed size

--- modules/shared/builder.nix
+++ modules/shared/builder.nix
@@ -1,9 +1,6 @@
 { pkgs, ... }:
-let
-  asGB = size: toString (size * 1024 * 1024 * 1024);
-in
 {
   nix.gc.options = ''
-    --max-freed "$((${asGB 50} * 1024**3 - 1024 * $(df -P -k /nix/store | tail -n 1 | ${pkgs.gawk}/bin/awk '{ print $4 }')))"
+    --max-freed "$((50 * 1024**3 - 1024 * $(df -P -k /nix/store | tail -n 1 | ${pkgs.gawk}/bin/awk '{ print $4 }')))"
   '';
 }
