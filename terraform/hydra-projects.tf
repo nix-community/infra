f2ea7aa87513d02f31a464fb71039df797676bef
zowoq <59103226+zowoq@users.noreply.github.com>
2023-09-19 15:23:18 +1000

    terraform/hydra-projects: keep one evaluation only for emacs_overlay
    https://github.com/nix-community/emacs-overlay/commit/755024956d0907f6252e33b722e4d38b3c9c3026

--- terraform/hydra-projects.tf
+++ terraform/hydra-projects.tf
@@ -49,7 +49,7 @@ resource "hydra_jobset" "emacs_overlay" {
 
   check_interval    = 1800
   scheduling_shares = 3000
-  keep_evaluations  = 3
+  keep_evaluations  = 1
 
   email_notifications = false
 }
