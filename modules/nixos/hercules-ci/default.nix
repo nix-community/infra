5ddef0745c5ee550433a05172c9c84d58a59bdb0
zowoq <59103226+zowoq@users.noreply.github.com>
2023-09-22 08:36:33 +1000

    modules/nixos/hercules-ci: re-add empty secrets file

--- modules/nixos/hercules-ci/default.nix
+++ modules/nixos/hercules-ci/default.nix
@@ -9,12 +9,15 @@ in
 {
   sops.secrets."binary-caches.json" = herculesSecret;
   sops.secrets."cluster-join-token.key" = herculesSecret;
+  sops.secrets."hercules-secrets" = herculesSecret;
 
   services.hercules-ci-agent = {
     enable = true;
     settings = {
       binaryCachesPath = secrets."binary-caches.json".path;
       clusterJoinTokenPath = secrets."cluster-join-token.key".path;
+      # secrets file is needed for effects
+      secretsJsonPath = secrets."hercules-secrets".path;
     };
   };
 }
