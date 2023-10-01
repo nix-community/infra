be49c75b4bd5ddbffbc651359436d809c244d0ce
zowoq <59103226+zowoq@users.noreply.github.com>
2023-10-01 12:24:12 +1000

    modules/darwin/common/upgrade-diff: remove `HOME` workaround
    https://github.com/LnL7/nix-darwin/commit/ab817e5d0e8561949dbc1a9640f88cde2990d862

--- modules/darwin/common/upgrade-diff.nix
+++ modules/darwin/common/upgrade-diff.nix
@@ -1,12 +1,10 @@
 { config, pkgs, ... }:
 {
-  # set $HOME to avoid 'warning: $HOME is not owned by you'
-  # https://github.com/NixOS/nix/issues/6834
   # srvos
   system.activationScripts.preActivation.text = ''
     if [[ -e /run/current-system ]]; then
       echo "--- diff to current-system"
-      HOME=/var/root ${pkgs.nvd}/bin/nvd --nix-bin-dir=${config.nix.package}/bin diff /run/current-system "$systemConfig"
+      ${pkgs.nvd}/bin/nvd --nix-bin-dir=${config.nix.package}/bin diff /run/current-system "$systemConfig"
       echo "---"
     fi
   '';
