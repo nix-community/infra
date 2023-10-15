4f2e1de15fd541651c26d3bc423eea31dca6002a
zowoq <59103226+zowoq@users.noreply.github.com>
2023-10-15 15:58:58 +1000

    terraform/shell: remove meta from terraform

--- terraform/shell.nix
+++ terraform/shell.nix
@@ -1,9 +1,13 @@
 { inputs', ... }:
+let
+  tf-pkgs = inputs'.tf-pkgs.legacyPackages;
+  terraform' = tf-pkgs.terraform.overrideAttrs (_: { meta = { }; });
+in
 {
   devShells = {
-    terraform = with inputs'.tf-pkgs.legacyPackages; mkShellNoCC {
+    terraform = with tf-pkgs; mkShellNoCC {
       packages = [
-        (terraform.withPlugins (p: [
+        (terraform'.withPlugins (p: [
           p.cloudflare
           p.external
           p.gandi
