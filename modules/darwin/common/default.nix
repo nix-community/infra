bb5b713d1db93d32fa367a97930ab8c033b3633e
zowoq <59103226+zowoq@users.noreply.github.com>
2023-11-11 08:38:00 +1000

    modules/darwin/common: add deployment key

--- modules/darwin/common/default.nix
+++ modules/darwin/common/default.nix
@@ -12,6 +12,7 @@
   # if user is removed the keys need to be removed manually from /etc/ssh/authorized_keys.d
   users.users = {
     hetzner.openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDPVjRBomWFJNNkZb0g5ymLmc3pdRddIScitmJ9yC+ap" # deployment
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKbBp2dH2X3dcU1zh+xW3ZsdYROKpJd3n13ssOP092qE" # mic92
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOuiDoBOxgyer8vGcfAIbE6TC4n4jo8lhG9l01iJ0bZz" # zimbatm
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFbCYwWByGE46XHH4Q0vZgQ5sOUgbH50M8KO2xhBC4m/" # zowoq
