c986d73104004ca2c10cbe3c1b9ab7207dc83170
zowoq <59103226+zowoq@users.noreply.github.com>
2023-12-03 11:49:32 +1000

    flake: remove nixpkgs-update nixConfig

--- flake.nix
+++ flake.nix
@@ -3,11 +3,9 @@
 
   nixConfig.extra-substituters = [
     "https://nix-community.cachix.org"
-    "https://nixpkgs-update.cachix.org"
   ];
   nixConfig.extra-trusted-public-keys = [
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    "nixpkgs-update.cachix.org-1:6y6Z2JdoL3APdu6/+Iy8eZX2ajf09e4EE9SnxSML1W8="
   ];
 
   inputs = {
