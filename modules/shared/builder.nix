cfb6fbf7f16a4f14d8ed1b5b80dfcc5a87ea46d0
zowoq <59103226+zowoq@users.noreply.github.com>
2023-12-07 11:31:39 +1000

    modules/shared/builder: add gc.gbFree option

--- modules/shared/builder.nix
+++ modules/shared/builder.nix
@@ -1,6 +1,12 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
 {
-  nix.gc.options = ''
-    --max-freed "$((50 * 1024**3 - 1024 * $(df -P -k /nix/store | tail -n 1 | ${pkgs.gawk}/bin/awk '{ print $4 }')))"
+  options.nixCommunity.gc.gbFree = lib.mkOption {
+    type = lib.types.int;
+    default = 50;
+    description = "Amount of free space in GB to keep on disk.";
+  };
+
+  config.nix.gc.options = ''
+    --max-freed "$((${toString config.nixCommunity.gc.gbFree} * 1024**3 - 1024 * $(df -P -k /nix/store | tail -n 1 | ${pkgs.gawk}/bin/awk '{ print $4 }')))"
   '';
 }
