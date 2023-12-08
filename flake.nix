f8784d80aedc492b436408466f23339ef9099806
zowoq <59103226+zowoq@users.noreply.github.com>
2023-12-08 18:00:48 +1000

    Revert "flake: disable buildbot nixosTest"
    This reverts commit 8d9da7e9a24fac70913e466922e2de87f7fcdcf1.

--- flake.nix
+++ flake.nix
@@ -81,8 +81,7 @@
               in
               darwinConfigurations // devShells // { inherit (self') formatter; } // nixosConfigurations // packages
               // pkgs.lib.optionalAttrs defaultPlatform {
-                # https://github.com/NixOS/nixpkgs/pull/271942
-                #nixosTests-buildbot = pkgs.nixosTests.buildbot;
+                nixosTests-buildbot = pkgs.nixosTests.buildbot;
                 nixosTests-buildbot-nix-master = inputs'.buildbot-nix.checks.master;
                 nixosTests-buildbot-nix-worker = inputs'.buildbot-nix.checks.worker;
                 nixosTests-hydra = pkgs.nixosTests.hydra.hydra_unstable;
