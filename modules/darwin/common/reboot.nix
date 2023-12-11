7c32bf2f2648f26a1f79eaafcb1a4ef9a7bf0398
zowoq <59103226+zowoq@users.noreply.github.com>
2023-12-11 11:21:35 +1000

    modules/darwin/common/reboot: add logs

--- modules/darwin/common/reboot.nix
+++ modules/darwin/common/reboot.nix
@@ -1,13 +1,20 @@
 {
   # reboot every sunday between 00:00 and 06:00
   launchd.daemons.reboot = {
-    script = ''/sbin/shutdown -r "+$(( $RANDOM % ( 6 * 60 ) ))"'';
-    serviceConfig.StartCalendarInterval = [
-      {
-        Hour = 0;
-        Minute = 0;
-        Weekday = 0;
-      }
-    ];
+    script = ''
+      date
+      /sbin/shutdown -r "+$(( $RANDOM % ( 6 * 60 ) ))"
+    '';
+    serviceConfig = {
+      StartCalendarInterval = [
+        {
+          Hour = 0;
+          Minute = 0;
+          Weekday = 0;
+        }
+      ];
+      StandardErrorPath = "/var/log/reboot.log";
+      StandardOutPath = "/var/log/reboot.log";
+    };
   };
 }
