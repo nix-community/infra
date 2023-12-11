ac560f2d4be58c0e6443a7ca8b94527c92d3d0ca
zowoq <59103226+zowoq@users.noreply.github.com>
2023-12-11 11:28:40 +1000

    build02: switch to new hardware

--- hosts/build02/configuration.nix
+++ hosts/build02/configuration.nix
@@ -8,20 +8,16 @@
     ./nixpkgs-update-backup.nix
     inputs.self.nixosModules.common
     inputs.self.nixosModules.builder
-    ./zfs.nix
+    inputs.self.nixosModules.disko-raid
   ];
 
-  # /boot is a mirror raid
-  boot.loader.grub.devices = [ "/dev/nvme0n1" "/dev/nvme1n1" ];
-  boot.loader.grub.enable = true;
-  boot.swraid.enable = true;
-  boot.loader.grub.extraConfig = "insmod mdraid1x";
+  nixCommunity.disko.raidLevel = 0; # more disk space, we don't have much state to restore anyway
 
   networking.hostName = "build02";
   networking.hostId = "af9ccc71";
   networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
 
-  systemd.network.networks."10-uplink".networkConfig.Address = "2a01:4f9:4a:2b02::1/64";
+  systemd.network.networks."10-uplink".networkConfig.Address = "2a01:4f9:3b:41d9::1";
 
-  system.stateVersion = "20.09";
+  system.stateVersion = "23.11";
 }
