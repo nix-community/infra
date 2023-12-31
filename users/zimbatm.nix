7655024111130a9832120a4037e26422f94560e0
zowoq <59103226+zowoq@users.noreply.github.com>
2023-12-31 12:42:56 +1000

    users: refactor keys

--- users/zimbatm.nix
+++ users/zimbatm.nix
@@ -1,13 +1,10 @@
 { lib, ... }:
 let
   userLib = import ./lib.nix { inherit lib; };
-  keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOuiDoBOxgyer8vGcfAIbE6TC4n4jo8lhG9l01iJ0bZz"
-  ];
 in
 {
   users.users.zimbatm = {
-    openssh.authorizedKeys.keys = keys;
+    openssh.authorizedKeys.keyFiles = [ ./keys/zimbatm ];
     useDefaultShell = true;
     isNormalUser = true;
     extraGroups = [
