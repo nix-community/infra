770c3d9ed4607d14fd8c907f6cbed0c579bb54b4
zowoq <59103226+zowoq@users.noreply.github.com>
2024-01-07 12:44:02 +1000

    modules/nixos/remote-workers: use ssh-ng

--- modules/nixos/remote-workers.nix
+++ modules/nixos/remote-workers.nix
@@ -4,11 +4,10 @@
 
   nix.distributedBuilds = true;
   nix.buildMachines = [
-    # these machines are used by hydra which doesn't support ssh-ng
     {
       hostName = "build04.nix-community.org";
       maxJobs = 80;
-      protocol = "ssh";
+      protocol = "ssh-ng";
       sshKey = config.sops.secrets.id_buildfarm.path;
       sshUser = "nix";
       system = "aarch64-linux";
@@ -22,7 +21,7 @@
     {
       hostName = "darwin02.nix-community.org";
       maxJobs = 8;
-      protocol = "ssh";
+      protocol = "ssh-ng";
       sshKey = config.sops.secrets.id_buildfarm.path;
       sshUser = "nix";
       systems = [ "aarch64-darwin" "x86_64-darwin" ];
