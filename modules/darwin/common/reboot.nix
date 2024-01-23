5cb6b931006ea439612a78259e5dfe5eaa2f1d7c
zowoq <59103226+zowoq@users.noreply.github.com>
2024-01-24 09:06:07 +1000

    modules/*/reboot: reduce window from 6 to 3 hours

--- modules/darwin/common/reboot.nix
+++ modules/darwin/common/reboot.nix
@@ -1,9 +1,9 @@
 {
-  # reboot every sunday between 00:00 and 06:00
+  # reboot every sunday between 00:00 and 03:00
   launchd.daemons.reboot = {
     script = ''
       date
-      /sbin/shutdown -r "+$(( $RANDOM % ( 6 * 60 ) ))"
+      /sbin/shutdown -r "+$(( $RANDOM % ( 3 * 60 ) ))"
     '';
     serviceConfig = {
       StartCalendarInterval = [
