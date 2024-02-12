4efafc7d88e4bd4ff7f4487e6ec1e8374fcf17c6
zowoq <59103226+zowoq@users.noreply.github.com>
2024-02-13 08:26:55 +1000

    flake: update nixpkgs-update

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
     # actually not used when using the modules but than nothing ever will try to fetch this nixpkgs variant
     srvos.inputs.nixpkgs.follows = "nixpkgs";
 
-    nixpkgs-update.url = "github:ryantm/nixpkgs-update";
+    nixpkgs-update.url = "github:rhendric/nixpkgs-update/rhendric/fix-388";
     nixpkgs-update.inputs.mmdoc.follows = "";
     nixpkgs-update-github-releases.url = "github:ryantm/nixpkgs-update-github-releases";
     nixpkgs-update-github-releases.flake = false;
