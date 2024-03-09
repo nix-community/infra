0dad55a059fd07e6046c4dc55bc8b5393f7c7a69
zowoq <59103226+zowoq@users.noreply.github.com>
2024-03-09 10:01:34 +1000

    darwin01: add darwin03 as builder

--- hosts/darwin01/configuration.nix
+++ hosts/darwin01/configuration.nix
@@ -5,6 +5,7 @@
     inputs.self.darwinModules.common
     inputs.self.darwinModules.builder
     inputs.self.darwinModules.community-builder
+    ./builders.nix
   ];
 
   nix.settings.sandbox = "relaxed";
--- hosts/darwin03/configuration.nix
+++ hosts/darwin03/configuration.nix
@@ -4,7 +4,11 @@
   imports = [
     inputs.self.darwinModules.common
     inputs.self.darwinModules.builder
+    inputs.self.darwinModules.remote-builder
   ];
+
+  # on nix-darwin if user is removed the keys need to be removed manually from /etc/ssh/authorized_keys.d
+  nixCommunity.remote-builder.key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKq4Jo8KcZEo3dcSBxFyaZA9Y8qWBLbOA/6aF6oqNYDS darwin-community-builder";
 
   nix.settings.sandbox = "relaxed";
   nix.settings.extra-platforms = [ "x86_64-darwin" ];
