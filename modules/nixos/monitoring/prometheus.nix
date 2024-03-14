2d3f24612585883908a34b7246e362ab650ae8d2
zowoq <59103226+zowoq@users.noreply.github.com>
2024-03-15 09:49:02 +1000

    modules/nixos/monitoring: ofborg: telegraf -> prometheus
    scraping this target with telegraf isn't working since 1.30.0

--- modules/nixos/monitoring/prometheus.nix
+++ modules/nixos/monitoring/prometheus.nix
@@ -25,6 +25,17 @@
             }
           ];
       }
+      {
+        job_name = "ofborg";
+        scrape_interval = "60s";
+        metrics_path = "/prometheus.php";
+        scheme = "https";
+        static_configs = [
+          {
+            targets = [ "events.ofborg.org" ];
+          }
+        ];
+      }
     ];
     alertmanagers = [
       {
