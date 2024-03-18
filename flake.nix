2e376f2ef57021eef1bef3fe3d0e698e0e3f925f
zowoq <59103226+zowoq@users.noreply.github.com>
2024-03-18 14:31:01 +1000

    flake: pin darwin02

--- flake.nix
+++ flake.nix
@@ -105,7 +105,8 @@
               modules = [ ./hosts/darwin01/configuration.nix ];
             };
             darwin02 = darwinSystem {
-              pkgs = inputs.nixpkgs.legacyPackages.aarch64-darwin;
+              # hercules-ci-agent: security: createProcess: posix_spawnp: does not exist
+              pkgs = inputs.nixpkgs-darwin-pinned.legacyPackages.aarch64-darwin;
               modules = [ ./hosts/darwin02/configuration.nix ];
             };
             darwin03 = darwinSystem {
