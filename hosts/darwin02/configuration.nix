c50f041203ac60f7191e04b0decf8c80b85b97fe
zowoq <59103226+zowoq@users.noreply.github.com>
2024-04-04 15:55:37 +1000

    darwin02: disable hercules, build03: set remotePlatformsWithSameFeatures for hercules

--- hosts/darwin02/configuration.nix
+++ hosts/darwin02/configuration.nix
@@ -4,7 +4,9 @@
   imports = [
     inputs.self.darwinModules.common
     inputs.self.darwinModules.builder
-    inputs.self.darwinModules.hercules-ci
+    # remotePlatformsWithSameFeatures is enabled on build03
+    # hercules-ci-agent: security: createProcess: posix_spawnp: does not exist
+    #inputs.self.darwinModules.hercules-ci
     inputs.self.darwinModules.remote-builder
   ];
 
