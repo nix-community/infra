cc30140779b42721690f18d02bda1fcd59a70df7
zowoq <59103226+zowoq@users.noreply.github.com>
2024-04-10 08:47:07 +1000

    modules/darwin/hercules-ci: add security to service path

--- modules/darwin/hercules-ci/default.nix
+++ modules/darwin/hercules-ci/default.nix
@@ -1,5 +1,15 @@
+{ config, pkgs, ... }:
+let
+  securityWrapper = pkgs.writeScriptBin "security" ''
+    exec /usr/bin/security "$@"
+  '';
+in
 {
   # hercules secrets are installed manually from ./secrets.yaml
   # https://docs.hercules-ci.com/hercules-ci/getting-started/deploy/nix-darwin
   services.hercules-ci-agent.enable = true;
+
+  # hercules-ci-agent: security: createProcess: posix_spawnp: does not exist
+  # https://github.com/LnL7/nix-darwin/blob/36524adc31566655f2f4d55ad6b875fb5c1a4083/modules/services/hercules-ci-agent/default.nix#L28
+  launchd.daemons.hercules-ci-agent.path = pkgs.lib.mkForce [ config.nix.package securityWrapper ];
 }
