17b28638cc62ec81528510a99a5c04b02a7c3e63
zowoq <59103226+zowoq@users.noreply.github.com>
2024-04-24 21:20:02 +1000

    modules/darwin/builder: set interactive ProcessType for nix daemon

--- modules/darwin/builder.nix
+++ modules/darwin/builder.nix
@@ -3,5 +3,8 @@
     ../shared/builder.nix
   ];
 
+  # https://github.com/LnL7/nix-darwin/blob/230a197063de9287128e2c68a7a4b0cd7d0b50a7/modules/nix/default.nix#L201
+  nix.daemonProcessType = "Interactive";
+
   nix.gc.interval = { Minute = 15; };
 }
