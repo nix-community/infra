413c46baf311bb9f25cb5c24011babfe1f7b7495
zowoq <59103226+zowoq@users.noreply.github.com>
2024-05-02 16:27:46 +1000

    modules/nixos: add disko-zfs-systemd-boot

---
+++ modules/nixos/disko-zfs-systemd-boot.nix
@@ -0,0 +1,77 @@
+{ inputs, lib, ... }:
+let
+  devices = idx: {
+    type = "disk";
+    device = "/dev/nvme${idx}n1";
+    content = {
+      type = "gpt";
+      partitions = {
+        ESP = lib.mkIf (idx == "0") {
+          size = "1G";
+          type = "EF00";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+            mountOptions = [ "nofail" ];
+          };
+        };
+        zfs = {
+          size = "100%";
+          content = {
+            type = "zfs";
+            pool = "zroot";
+          };
+        };
+      };
+    };
+  };
+in
+{
+  imports = [ inputs.disko.nixosModules.disko ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  disko.devices = {
+    disk = {
+      x = devices "0";
+      y = devices "1";
+    };
+    zpool = {
+      zroot = {
+        type = "zpool";
+        options = {
+          ashift = "12";
+        };
+        rootFsOptions = {
+          acltype = "posix";
+          atime = "off";
+          compression = "lz4";
+          mountpoint = "none";
+          xattr = "sa";
+          "com.sun:auto-snapshot" = "false";
+        };
+        datasets = {
+          root = {
+            type = "zfs_fs";
+            mountpoint = "/";
+            options.mountpoint = "/";
+          };
+          build = {
+            type = "zfs_fs";
+            mountpoint = "/nix/var/nix/builds";
+            options.mountpoint = "/nix/var/nix/builds";
+            options.sync = "disabled";
+          };
+          tmp = {
+            type = "zfs_fs";
+            mountpoint = "/tmp";
+            options.mountpoint = "/tmp";
+            options.sync = "disabled";
+          };
+        };
+      };
+    };
+  };
+}
