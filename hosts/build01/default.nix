587d4a6412c98aaa48772e57d7cb31cb634a65e7
zowoq <59103226+zowoq@users.noreply.github.com>
2024-05-02 16:28:16 +1000

    build01: switch to zfs with systemd-boot

--- hosts/build01/default.nix
+++ hosts/build01/default.nix
@@ -3,7 +3,7 @@
   imports = [
     inputs.self.nixosModules.cgroups
     inputs.self.nixosModules.community-builder
-    inputs.self.nixosModules.disko-zfs
+    inputs.self.nixosModules.disko-zfs-systemd-boot
     inputs.srvos.nixosModules.hardware-hetzner-online-amd
   ];
 
