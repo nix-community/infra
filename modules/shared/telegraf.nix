d9627084ca5540c038514c2d17a85bc7f25d5949
zowoq <59103226+zowoq@users.noreply.github.com>
2024-05-04 18:54:17 +1000

    modules/shared/telegraf: add /etc/flake-inputs.prom
    allows the flake inputs to be changed without restarting telegraf

--- modules/shared/telegraf.nix
+++ modules/shared/telegraf.nix
@@ -1,5 +1,30 @@
+{ inputs, lib, ... }:
 {
+  environment.etc =
+    let
+      inputsWithDate = lib.filterAttrs (_: input: input ? lastModified) inputs.self.inputs;
+      flakeAttrs = input: (lib.mapAttrsToList (n: v: ''${n}="${v}"'')
+        (lib.filterAttrs (_: v: (builtins.typeOf v) == "string") input));
+      lastModified = name: input: ''
+        flake_input_last_modified{input="${name}",${lib.concatStringsSep "," (flakeAttrs input)}} ${toString input.lastModified}'';
+    in
+    {
+      "flake-inputs.prom" = {
+        text = builtins.unsafeDiscardStringContext ''
+          # HELP flake_registry_last_modified Last modification date of flake input in unixtime
+          # TYPE flake_input_last_modified gauge
+          ${lib.concatStringsSep "\n" (lib.mapAttrsToList lastModified inputsWithDate)}
+        '';
+      };
+    };
+
   services.telegraf.extraConfig.inputs = {
+    file = [
+      {
+        data_format = "prometheus";
+        files = [ "/etc/flake-inputs.prom" ];
+      }
+    ];
     prometheus = {
       metric_version = 2;
     };
