654f4190df41cc8ad4cd221562c097ff64d8db63
zowoq <59103226+zowoq@users.noreply.github.com>
2024-05-09 15:14:57 +1000

    modules/nixos/common/reboot: add kexec

--- modules/nixos/common/reboot.nix
+++ modules/nixos/common/reboot.nix
@@ -1,15 +1,20 @@
 { config, pkgs, ... }:
 {
-  # adapted from https://github.com/NixOS/nixpkgs/blob/3428bdf3c93a7608615dddd44dec50c3df89b4be/nixos/modules/tasks/auto-upgrade.nix
+  # adapted from:
+  # https://github.com/NixOS/nixpkgs/blob/3428bdf3c93a7608615dddd44dec50c3df89b4be/nixos/modules/system/boot/kexec.nix
+  # https://github.com/NixOS/nixpkgs/blob/3428bdf3c93a7608615dddd44dec50c3df89b4be/nixos/modules/tasks/auto-upgrade.nix
   systemd.services.reboot-after-update = {
     restartIfChanged = false;
     unitConfig.X-StopOnRemoval = false;
     serviceConfig.Type = "oneshot";
-    path = [ config.systemd.package pkgs.coreutils ];
+    path = [ config.systemd.package pkgs.coreutils pkgs.kexec-tools ];
     script = ''
       booted="$(readlink /run/booted-system/{initrd,kernel,kernel-modules} && cat /run/booted-system/kernel-params)"
-      built="$(readlink /nix/var/nix/profiles/system/{initrd,kernel,kernel-modules} && cat /nix/var/nix/profiles/system/kernel-params)"
+      p="$(readlink -f /nix/var/nix/profiles/system)"
+      built="$(readlink $p/{initrd,kernel,kernel-modules} && cat $p/kernel-params)"
       if [ "''${booted}" != "''${built}" ]; then
+        # don't use kexec if system is virtualized
+        systemd-detect-virt -q || kexec --load $p/kernel --initrd=$p/initrd --append="$(cat $p/kernel-params) init=$p/init"
         systemctl reboot
       fi
     '';
