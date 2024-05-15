319e1787e27028154c8c4a6f46cd8cde560a5ce7
zowoq <59103226+zowoq@users.noreply.github.com>
2024-05-15 21:14:55 +1000

    darwin03: convert to CI builder

--- hosts/build03/builders.nix
+++ hosts/build03/builders.nix
@@ -22,5 +22,14 @@
       systems = [ "aarch64-darwin" "x86_64-darwin" ];
       supportedFeatures = inputs.self.outputs.darwinConfigurations.darwin02.config.nix.settings.system-features;
     }
+    {
+      hostName = "darwin03.nix-community.org";
+      maxJobs = 8;
+      protocol = "ssh-ng";
+      sshKey = config.sops.secrets.id_buildfarm.path;
+      sshUser = "nix";
+      systems = [ "aarch64-darwin" "x86_64-darwin" ];
+      supportedFeatures = inputs.self.outputs.darwinConfigurations.darwin03.config.nix.settings.system-features;
+    }
   ];
 }
