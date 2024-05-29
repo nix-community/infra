e40f71899c4d877e1070aad3df943801b576ccc2
Artturin <Artturin@artturin.com>
2024-05-30 00:08:19 +0300

    modules/nixos/community-builder: add a sane zsh config
    Biggest benefits are better prompt and the ability to properly use arrow up
    to go through the history
    
    `nix<up>` searches through commands which start with `nix`
    
    https://grml.org/

--- modules/nixos/community-builder/default.nix
+++ modules/nixos/community-builder/default.nix
@@ -8,5 +8,13 @@
   # disable generated completion
   environment.etc."fish/generated_completions".text = pkgs.lib.mkForce "";
 
-  programs.zsh.enable = true;
+  programs.zsh = {
+    enable = true;
+    # https://grml.org/zsh/grmlzshrc.html
+    # https://grml.org/zsh/grml-zsh-refcard.pdf
+    interactiveShellInit = ''
+      source ${pkgs.grml-zsh-config}/etc/zsh/zshrc
+    '';
+    promptInit = ""; # otherwise it'll override the grml prompt
+  };
 }
