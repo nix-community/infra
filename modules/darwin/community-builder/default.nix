9eb6a7e53aa9f5be801441bdbba0af722756ef9b
Artturin <Artturin@artturin.com>
2024-05-30 00:03:07 +0300

    modules/darwin/community-builder: add a sane zsh config
    Biggest benefits are better prompt and the ability to properly use arrow up
    to go through the history
    
    `nix<up>` searches through commands which start with `nix`
    
    https://grml.org/

--- modules/darwin/community-builder/default.nix
+++ modules/darwin/community-builder/default.nix
@@ -1,6 +1,16 @@
+{ pkgs, ... }:
 {
   imports = [
     ./packages.nix
     ./users.nix
   ];
+
+  programs.zsh = {
+    # https://grml.org/zsh/grmlzshrc.html
+    # https://grml.org/zsh/grml-zsh-refcard.pdf
+    interactiveShellInit = ''
+      source ${pkgs.grml-zsh-config}/etc/zsh/zshrc
+    '';
+    promptInit = ""; # otherwise it'll override the grml prompt
+  };
 }
