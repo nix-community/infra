1756f404c14ac47b88bf70781204f7f31825c4a9
zowoq <59103226+zowoq@users.noreply.github.com>
2024-06-10 13:10:25 +1000

    modules/darwin/hercules-ci: add sandbox workaround

--- modules/darwin/hercules-ci/default.nix
+++ modules/darwin/hercules-ci/default.nix
@@ -20,4 +20,8 @@
     binaryCachesPath = config.age.secrets.binary-caches.path;
     clusterJoinTokenPath = config.age.secrets.cluster-join-token.path;
   };
+
+  system.systemBuilderArgs.sandboxProfile = ''
+    (allow file-read* file-write* process-exec mach-lookup (subpath "${builtins.storeDir}"))
+  '';
 }
