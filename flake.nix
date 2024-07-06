12de0d0624f3726ba3f219f6ce498faf63f461c0
zowoq <59103226+zowoq@users.noreply.github.com>
2024-07-06 15:17:11 +1000

    modules/nixos: remove disko-raid

--- flake.nix
+++ flake.nix
@@ -157,7 +157,6 @@
           buildbot = ./modules/nixos/buildbot.nix;
           builder = ./modules/nixos/builder.nix;
           community-builder = ./modules/nixos/community-builder;
-          disko-raid = ./modules/nixos/disko-raid.nix;
           disko-zfs = ./modules/nixos/disko-zfs.nix;
           github-org-backup = ./modules/nixos/github-org-backup.nix;
           hercules-ci = ./modules/nixos/hercules-ci;
