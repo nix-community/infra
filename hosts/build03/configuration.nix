c3d047dde3a5763999ef3a5635f0e92b289d35bd
zowoq <59103226+zowoq@users.noreply.github.com>
2024-07-09 11:08:35 +1000

    build02, build03: set gc to 500gb

--- hosts/build03/configuration.nix
+++ hosts/build03/configuration.nix
@@ -17,6 +17,8 @@
     ./postgresql.nix
   ];
 
+  nixCommunity.gc.gbFree = 500;
+
   systemd.network.networks."10-uplink".networkConfig.Address = "2a01:4f9:3b:2946::1/64";
 
   networking.hostName = "build03";
