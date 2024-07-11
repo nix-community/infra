78cf495e214308cab643682fc892fc43569dc2bb
Jörg Thalheim <joerg@thalheim.io>
2024-07-12 00:12:49 +0200

    make it a shell instead of ssh
    I had issues with the network but than didn't bothered further because
    the terminal access was good enough.

--- flake.nix
+++ flake.nix
@@ -161,7 +161,7 @@
           github-org-backup = ./modules/nixos/github-org-backup.nix;
           hercules-ci = ./modules/nixos/hercules-ci.nix;
           hydra = ./modules/nixos/hydra.nix;
-          initrd-ssh = ./modules/nixos/initrd-ssh.nix;
+          emergency-access = ./modules/nixos/emergency-access.nix;
           monitoring = ./modules/nixos/monitoring;
           nur-update = ./modules/nixos/nur-update.nix;
           remote-builder = ./modules/nixos/remote-builder.nix;
