042e0689ced04b451e30cee22595800359d43696
zowoq <59103226+zowoq@users.noreply.github.com>
2024-07-25 20:39:21 +1000

    modules/nixos/common: add initrd ssh

--- modules/nixos/common/security.nix
+++ modules/nixos/common/security.nix
@@ -1,9 +1,15 @@
+{ inputs, pkgs, ... }:
 {
   # Make sure that the firewall is enabled, even if it's the default.
   networking.firewall.enable = true;
 
   # allow to access emergency shell with a password
   boot.initrd.systemd.emergencyAccess = "$6$he2fblfl/H7I.kvz$WbSCMXu8ztmqfj5jG4czqvu/rkMHxufxqHgy1urzXFSN.jZB4QiW5lOjR08vk8pZTyim3TT1wFkMaNE9zZ3sc1";
+
+  boot.initrd.network.ssh = {
+    enable = true;
+    authorizedKeyFiles = pkgs.lib.filesystem.listFilesRecursive "${toString inputs.self}/users/keys";
+  };
 
   services.openssh = {
     hostKeys = [
