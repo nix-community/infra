3f0c7bba3c2942874d4880b73f697f7c11f7f4fb
zowoq <59103226+zowoq@users.noreply.github.com>
2024-07-31 12:32:39 +1000

    modules/nixos/hydra: set max-jobs for builders

--- modules/nixos/hydra.nix
+++ modules/nixos/hydra.nix
@@ -30,12 +30,13 @@
       # remote builders set in /etc/nix/machines + localhost
       buildMachinesFiles = [
         (pkgs.runCommand "etc-nix-machines" { machines = config.environment.etc."nix/machines".text; } ''
-          printf "$machines" > $out
-          substituteInPlace $out --replace 'ssh-ng://' 'ssh://'
+          printf "$machines" | grep build04 > $out
+          substituteInPlace $out --replace-fail 'ssh-ng://' 'ssh://'
+          substituteInPlace $out --replace-fail ' 80 ' ' 2 '
         '')
 
         (pkgs.writeText "local" ''
-          localhost x86_64-linux,builtin - 8 1 ${pkgs.lib.concatStringsSep "," config.nix.settings.system-features} - -
+          localhost x86_64-linux,builtin - 2 1 ${pkgs.lib.concatStringsSep "," config.nix.settings.system-features} - -
         '')
       ];
       hydraURL = "https://hydra.nix-community.org";
