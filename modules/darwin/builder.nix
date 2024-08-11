db3e6fc921689e53eae6c45fc041cbdee6189c15
zowoq <59103226+zowoq@users.noreply.github.com>
2024-08-11 12:05:42 +1000

    modules/builder: gc by percent used

--- modules/darwin/builder.nix
+++ modules/darwin/builder.nix
@@ -1,10 +1,23 @@
 {
-  imports = [ ../shared/builder.nix ];
+  config,
+  inputs,
+  pkgs,
+  ...
+}:
+{
+  nix.gc.automatic = false;
+
+  launchd.daemons.free-space = {
+    serviceConfig = {
+      StartCalendarInterval = [ { Minute = 15; } ];
+    };
+    path = [
+      config.nix.package
+      pkgs.coreutils
+    ];
+    script = import "${inputs.self}/modules/shared/free-space.nix";
+  };
 
   # https://github.com/LnL7/nix-darwin/blob/230a197063de9287128e2c68a7a4b0cd7d0b50a7/modules/nix/default.nix#L201
   nix.daemonProcessType = "Interactive";
-
-  nix.gc.interval = {
-    Minute = 15;
-  };
 }
