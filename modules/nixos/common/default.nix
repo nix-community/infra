9a2c56884974b0eece3698d976ee10c681d16cea
zowoq <59103226+zowoq@users.noreply.github.com>
2024-09-02 10:05:38 +1000

    modules/nixos/common: optimise zram

--- modules/nixos/common/default.nix
+++ modules/nixos/common/default.nix
@@ -40,6 +40,15 @@
 
   zramSwap.enable = true;
 
+  # https://github.com/NixOS/nixpkgs/pull/268121
+  # https://wiki.archlinux.org/title/Zram#Optimizing_swap_on_zram
+  boot.kernel.sysctl = {
+    "vm.page-cluster" = 0;
+    "vm.swappiness" = 180;
+    "vm.watermark_boost_factor" = 0;
+    "vm.watermark_scale_factor" = 125;
+  };
+
   security.acme.defaults.email = "trash@nix-community.org";
   security.acme.acceptTerms = true;
 
