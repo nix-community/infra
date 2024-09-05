4fa7e5b5cee87ac91ba460bbe23a5fbce3073d71
zowoq <59103226+zowoq@users.noreply.github.com>
2024-09-05 17:59:59 +1000

    modules/community-builder: refactor

--- modules/darwin/community-builder/default.nix
+++ modules/darwin/community-builder/default.nix
@@ -1,16 +1,11 @@
-{ pkgs, ... }:
+{ inputs, pkgs, ... }:
 {
   imports = [
-    ./packages.nix
+    "${inputs.self}/modules/shared/community-builder.nix"
     ./users.nix
   ];
 
-  programs.zsh = {
-    # https://grml.org/zsh/grmlzshrc.html
-    # https://grml.org/zsh/grml-zsh-refcard.pdf
-    interactiveShellInit = ''
-      source ${pkgs.grml-zsh-config}/etc/zsh/zshrc
-    '';
-    promptInit = ""; # otherwise it'll override the grml prompt
-  };
+  environment.systemPackages = [
+    pkgs.vim
+  ];
 }
