a76796be60c8f7bc37e4f13c34fe1ed6cb83e321
zowoq <59103226+zowoq@users.noreply.github.com>
2024-09-17 09:31:40 +1000

    modules/community-builder: add nixpkgs clone
    Co-authored-by: nicoo <nicoo@mur.at>

--- modules/darwin/community-builder/default.nix
+++ modules/darwin/community-builder/default.nix
@@ -1,4 +1,9 @@
-{ inputs, pkgs, ... }:
+{
+  config,
+  inputs,
+  pkgs,
+  ...
+}:
 {
   imports = [
     "${inputs.self}/modules/shared/community-builder.nix"
@@ -9,4 +14,24 @@
   environment.systemPackages = [
     pkgs.vim
   ];
+
+  launchd.daemons.nixpkgs-clone = {
+    environment = {
+      inherit (config.environment.variables) NIX_SSL_CERT_FILE;
+    };
+    serviceConfig = {
+      StartCalendarInterval = [
+        {
+          Hour = 0;
+          Minute = 0;
+        }
+      ];
+      StandardErrorPath = "/var/log/nixpkgs-clone.log";
+      StandardOutPath = "/var/log/nixpkgs-clone.log";
+    };
+    path = [
+      pkgs.git
+    ];
+    script = builtins.readFile "${inputs.self}/modules/shared/nixpkgs-clone.bash";
+  };
 }
