49f65405441ebb089503b71abd706cdf7e974c4c
zowoq <59103226+zowoq@users.noreply.github.com>
2024-09-24 10:31:46 +1000

    modules/community-builder: add motd

--- modules/shared/community-builder.nix
+++ modules/shared/community-builder.nix
@@ -1,26 +1,43 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 {
-  # useful for people that want to test stuff
-  environment.systemPackages = [
-    pkgs.fd
-    pkgs.git
-    pkgs.nano
-    pkgs.nix-output-monitor
-    pkgs.nix-tree
-    pkgs.nixpkgs-review
-    pkgs.ripgrep
-    pkgs.tig
-  ];
+  options.nixCommunity.motd = lib.mkOption {
+    type = lib.types.str;
+    description = "message of the day";
+  };
 
-  programs.nix-index-database.comma.enable = true;
+  config = {
+    nixCommunity.motd = ''
 
-  programs.zsh = {
-    enable = true;
-    # https://grml.org/zsh/grmlzshrc.html
-    # https://grml.org/zsh/grml-zsh-refcard.pdf
-    interactiveShellInit = ''
-      source ${pkgs.grml-zsh-config}/etc/zsh/zshrc
+      Welcome to Nix Community!
+
+      For a faster Nixpkgs clone use:
+
+      git clone --reference /var/lib/nixpkgs.git https://github.com/NixOS/nixpkgs.git
+
     '';
-    promptInit = ""; # otherwise it'll override the grml prompt
+
+    # useful for people that want to test stuff
+    environment.systemPackages = [
+      pkgs.fd
+      pkgs.git
+      pkgs.nano
+      pkgs.nix-output-monitor
+      pkgs.nix-tree
+      pkgs.nixpkgs-review
+      pkgs.ripgrep
+      pkgs.tig
+    ];
+
+    programs.nix-index-database.comma.enable = true;
+
+    programs.zsh = {
+      enable = true;
+      # https://grml.org/zsh/grmlzshrc.html
+      # https://grml.org/zsh/grml-zsh-refcard.pdf
+      interactiveShellInit = ''
+        source ${pkgs.grml-zsh-config}/etc/zsh/zshrc
+      '';
+      promptInit = ""; # otherwise it'll override the grml prompt
+    };
   };
 }
