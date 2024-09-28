ea67192e39009aa9959deb385c2864902b728187
zowoq <59103226+zowoq@users.noreply.github.com>
2024-09-28 10:59:07 +1000

    modules/shared/telegraf: move host-info to bash script

---
+++ modules/shared/host-info.bash
@@ -0,0 +1,12 @@
+flake=$(nix flake metadata self --json | jq -r '.path' | sed -e 's|/nix/store/||' -e 's|-source||')
+nix_version="$(nix store ping --store daemon --json | jq -r '.version')"
+case "$(uname -s)" in
+Darwin)
+  os_version="$(/usr/bin/sw_vers --productVersion)_$(/usr/bin/sw_vers --buildVersion)"
+  ;;
+Linux)
+  os_version="$(uname -r)"
+  ;;
+esac
+system="$(nix eval --impure --raw --expr 'builtins.currentSystem')"
+echo "host,flake=$flake,nix_version=$nix_version,os_version=$os_version,system=$system info=1"
