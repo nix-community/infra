7f84e4ff4854c4f30ae0b57483691417abfbbee7
zowoq <59103226+zowoq@users.noreply.github.com>
2024-09-28 10:58:49 +1000

    modules/shared: move free-space to bash script

--- modules/darwin/builder.nix
+++ modules/darwin/builder.nix
@@ -15,7 +15,7 @@
       config.nix.package
       pkgs.coreutils
     ];
-    script = import "${inputs.self}/modules/shared/free-space.nix";
+    script = builtins.readFile "${inputs.self}/modules/shared/free-space.bash";
   };
 
   # https://github.com/LnL7/nix-darwin/blob/230a197063de9287128e2c68a7a4b0cd7d0b50a7/modules/nix/default.nix#L201
