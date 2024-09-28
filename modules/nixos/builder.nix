c17f5a7529cd7441565b204d307e47277b78a026
zowoq <59103226+zowoq@users.noreply.github.com>
2024-09-28 10:58:49 +1000

    modules/shared: move free-space to bash script

--- modules/nixos/builder.nix
+++ modules/nixos/builder.nix
@@ -14,7 +14,7 @@
       config.nix.package
       pkgs.coreutils
     ];
-    script = import "${inputs.self}/modules/shared/free-space.nix";
+    script = builtins.readFile "${inputs.self}/modules/shared/free-space.bash";
   };
 
   # Bump the open files limit so that non-root users can run NixOS VM tests
