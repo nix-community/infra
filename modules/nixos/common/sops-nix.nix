9746ffa853f241dbed77ec9a5ca3fc6951a71a5c
zowoq <59103226+zowoq@users.noreply.github.com>
2024-09-30 16:40:00 +1000

    modules/nixos/common: refactor

--- modules/nixos/common/sops-nix.nix
+++ modules/nixos/common/sops-nix.nix
@@ -8,5 +8,9 @@ let
   defaultSopsPath = "${toString inputs.self}/hosts/${config.networking.hostName}/secrets.yaml";
 in
 {
+  imports = [
+    inputs.sops-nix.nixosModules.sops
+  ];
+
   sops.defaultSopsFile = lib.mkIf (builtins.pathExists defaultSopsPath) defaultSopsPath;
 }
