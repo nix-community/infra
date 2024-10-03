64675a0aa065594e17418b4167b028e809e61eaf
zowoq <59103226+zowoq@users.noreply.github.com>
2024-10-04 08:08:29 +1000

    modules/darwin: switch to srvos server

--- modules/darwin/common/default.nix
+++ modules/darwin/common/default.nix
@@ -1,45 +1,24 @@
 { inputs, ... }:
 {
   imports = [
+    inputs.srvos.darwinModules.server
     ./apfs-cleanup.nix
     ./network.nix
-    ./optimise.nix
     ./packages.nix
     ./reboot.nix
     ./software-update.nix
     ./telegraf.nix
-    ./upgrade-diff.nix
     ./users.nix
     ../../shared/known-hosts.nix
     ../../shared/nix-daemon.nix
     inputs.agenix.darwinModules.age
   ];
 
-  services.nix-daemon.enable = true;
+  # https://github.com/NixOS/nix/issues/7273
+  nix.settings.auto-optimise-store = false;
 
-  programs.zsh.enable = true;
-
-  documentation.enable = false;
-
-  programs.info.enable = false;
-
-  # srvos
-  nix.settings.builders-use-substitutes = true;
-
-  # srvos
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
-
-  system.includeUninstaller = false;
-
-  # srvos
   environment.etc."ssh/sshd_config.d/darwin.conf".text = ''
-    AuthorizedKeysFile none
     HostKey /etc/ssh/ssh_host_ed25519_key
-    KbdInteractiveAuthentication no
-    PasswordAuthentication no
   '';
 
   system.activationScripts.postActivation.text = ''
@@ -47,6 +26,4 @@
     mdutil -a -i off -d &> /dev/null
     mdutil -a -E &> /dev/null
   '';
-
-  time.timeZone = "GMT";
 }
