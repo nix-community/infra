83834e3f6e112186b1e6167b31f854575e646cd8
zowoq <59103226+zowoq@users.noreply.github.com>
2024-10-03 22:02:05 +1000

    modules/darwin: switch to srvos telegraf

--- modules/darwin/common/telegraf.nix
+++ modules/darwin/common/telegraf.nix
@@ -1,41 +1,7 @@
-{ pkgs, ... }:
+{ inputs, ... }:
 {
-  imports = [ ../../shared/telegraf.nix ];
-
-  services.telegraf = {
-    enable = true;
-    extraConfig = {
-      agent.interval = "60s";
-      inputs = {
-        smart.path_smartctl = "${pkgs.smartmontools}/bin/smartctl";
-        system = { };
-        mem = { };
-        swap = { };
-        disk.tagdrop = {
-          fstype = [
-            "tmpfs"
-            "ramfs"
-            "devtmpfs"
-            "devfs"
-            "iso9660"
-            "overlay"
-            "aufs"
-            "squashfs"
-          ];
-          device = [
-            "rpc_pipefs"
-            "lxcfs"
-            "nsfs"
-            "borgfs"
-          ];
-        };
-        diskio = { };
-        internal = { };
-      };
-      outputs.prometheus_client = {
-        listen = ":9273";
-        metric_version = 2;
-      };
-    };
-  };
+  imports = [
+    inputs.srvos.darwinModules.mixins-telegraf
+    ../../shared/telegraf.nix
+  ];
 }
