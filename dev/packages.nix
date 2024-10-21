78289fa2935c48d8cad43e693a5541b64323298b
zowoq <59103226+zowoq@users.noreply.github.com>
2024-10-22 09:31:01 +1000

    modules/darwin/community-builder: add SecurityKeyProvider

--- dev/packages.nix
+++ dev/packages.nix
@@ -10,6 +10,19 @@
     src = inputs.hydra;
     buildInputs = o.buildInputs ++ [ final.perlPackages.DBIxClassHelpers ];
   });
+  sk-libfido2 = prev.openssh.overrideAttrs (o: {
+    pname = "sk-libfido2";
+    # rebase of https://github.com/openssh/openssh-portable/commit/ca0697a90e5720ba4d76cb0ae9d5572b5260a16c
+    patches = o.patches ++ [ ./sk-libfido2.patch ];
+    configureFlags = o.configureFlags ++ [ "--with-security-key-standalone" ];
+    buildFlags = [
+      "PATHS="
+      "SK_STANDALONE=sk-libfido2.dylib"
+    ];
+    installPhase = "install sk-libfido2.dylib -Dt $out";
+    postInstall = null;
+    doCheck = false;
+  });
   sotp = final.buildGoModule rec {
     pname = "sotp";
     version = "e7f7c804b1641169ce850d8352fb07294881609e";
