0682c6118658b57f71a9efaaca27b80b5673bc57
zowoq <59103226+zowoq@users.noreply.github.com>
2024-10-29 10:05:51 +1000

    modules/shared/community-builder: add github token to nix.conf

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -11,13 +11,21 @@ let
 
   inherit ((import ../modules/shared/known-hosts.nix).programs.ssh) knownHosts;
 
+  build01 = knownHosts.build01.publicKey;
   build02 = knownHosts.build02.publicKey;
   build03 = knownHosts.build03.publicKey;
   build04 = knownHosts.build04.publicKey;
+  darwin01 = knownHosts.darwin01.publicKey;
   darwin02 = knownHosts.darwin02.publicKey;
   web02 = knownHosts.web02.publicKey;
 
   secrets = {
+    # fine-grained, no permissions github token, expires 2025-10-29
+    # from `nix-community-buildbot` (user account, not the github app)
+    community-builder-nix-access-tokens = [
+      build01
+      darwin01
+    ];
     grafana-client-secret = [ web02 ];
     hercules-binary-caches = [
       build03
