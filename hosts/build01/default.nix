975ff47dc7107ede34318f2e72d4338d7071080b
zowoq <59103226+zowoq@users.noreply.github.com>
2024-11-04 08:57:49 +1000

    build01: preferStaticEmulators for binfmt

--- hosts/build01/default.nix
+++ hosts/build01/default.nix
@@ -9,6 +9,7 @@
 
   # Emulate riscv64 until we have proper builders
   boot.binfmt.emulatedSystems = [ "riscv64-linux" ];
+  boot.binfmt.preferStaticEmulators = true;
 
   systemd.network.networks."10-uplink".networkConfig.Address = "2a01:4f9:3b:2946::1/64";
 
