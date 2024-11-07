29335253b42805e14c8bd14585e0fce3a6c0daff
zowoq <59103226+zowoq@users.noreply.github.com>
2024-11-07 13:18:06 +1000

    Revert "modules/shared/community-builder: disable termite"
    This reverts commit d56f267bdf880785c90d5c9d49eb49986b3ea7b3.

--- modules/shared/community-builder.nix
+++ modules/shared/community-builder.nix
@@ -52,7 +52,7 @@
       ++ builtins.filter (lib.meta.availableOn pkgs.stdenv.hostPlatform) [
         pkgs.foot.terminfo
         pkgs.kitty.terminfo
-        #pkgs.termite.terminfo
+        pkgs.termite.terminfo
         pkgs.wezterm.terminfo
       ];
 
