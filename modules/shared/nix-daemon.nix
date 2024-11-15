4c623cfb5f94e8b2d29536e1a3c5778014fbdc22
zowoq <59103226+zowoq@users.noreply.github.com>
2024-11-16 09:35:29 +1000

    modules/shared/nix-daemon: set max-silent-time, timeout

--- modules/shared/nix-daemon.nix
+++ modules/shared/nix-daemon.nix
@@ -40,5 +40,10 @@ in
 
     gc.automatic = pkgs.lib.mkDefault true;
     gc.options = pkgs.lib.mkDefault "--delete-older-than 14d";
+
+    # match buildbot timeouts
+    # https://github.com/nix-community/buildbot-nix/blob/85c0b246cc96cc244e4d9889a97c4991c4593dc3/buildbot_nix/__init__.py#L1008
+    settings.max-silent-time = toString (60 * 20);
+    settings.timeout = toString (60 * 60 * 3);
   };
 }
