272bb5ffc1e241d1d84ca586408b788f5e69d511
zowoq <59103226+zowoq@users.noreply.github.com>
2024-11-21 12:51:20 +1000

    sort imports

--- hosts/build03/default.nix
+++ hosts/build03/default.nix
@@ -1,21 +1,20 @@
 { inputs, ... }:
 {
   imports = [
-    inputs.self.nixosModules.nginx
-    inputs.srvos.nixosModules.hardware-hetzner-online-amd
-    inputs.self.nixosModules.disko-zfs
+    ./builders.nix
+    ./postgresql.nix
     inputs.self.nixosModules.buildbot
     inputs.self.nixosModules.builder
-    inputs.self.nixosModules.ci-builder
     inputs.self.nixosModules.cgroups
-    inputs.self.nixosModules.hercules-ci
-    inputs.self.nixosModules.watch-store
-    ./builders.nix
-
+    inputs.self.nixosModules.ci-builder
+    inputs.self.nixosModules.disko-zfs
     inputs.self.nixosModules.github-org-backup
+    inputs.self.nixosModules.hercules-ci
     inputs.self.nixosModules.hydra
+    inputs.self.nixosModules.nginx
     inputs.self.nixosModules.nur-update
-    ./postgresql.nix
+    inputs.self.nixosModules.watch-store
+    inputs.srvos.nixosModules.hardware-hetzner-online-amd
   ];
 
   systemd.network.networks."10-uplink".networkConfig.Address = "2a01:4f8:2190:2698::2";
