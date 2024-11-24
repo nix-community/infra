3408dbcfea194a2288eddfc7be00f9fe86cf1875
zowoq <59103226+zowoq@users.noreply.github.com>
2024-11-24 11:31:47 +1000

    modules/nixos/monitoring: switch to oauth-proxy for alertmanager

--- modules/nixos/monitoring/default.nix
+++ modules/nixos/monitoring/default.nix
@@ -1,24 +1,17 @@
-{ config, inputs, ... }:
+{ inputs, ... }:
 {
   imports = [
     inputs.srvos.nixosModules.roles-prometheus
     ./alert-rules.nix
     ./grafana.nix
     ./matrix-hook.nix
+    ./oauth2-proxy.nix
     ./prometheus.nix
     ./telegraf.nix
   ];
 
-  age.secrets.nginx-basic-auth-file = {
-    file = "${inputs.self}/secrets/nginx-basic-auth-file.age";
-    owner = "nginx";
-  };
-
   services.nginx.virtualHosts."alertmanager.nix-community.org" = {
-    locations."/" = {
-      basicAuthFile = config.age.secrets.nginx-basic-auth-file.path;
-      proxyPass = "http://localhost:9093/";
-    };
+    locations."/".proxyPass = "http://localhost:9093/";
   };
   services.nginx.virtualHosts."grafana.nix-community.org" = {
     locations."/" = {
