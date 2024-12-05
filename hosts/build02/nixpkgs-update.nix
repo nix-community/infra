fcc691c72013eac08615f69890d896e79b138e6a
zowoq <59103226+zowoq@users.noreply.github.com>
2024-12-05 14:31:22 +1000

    worker

--- hosts/build02/nixpkgs-update.nix
+++ hosts/build02/nixpkgs-update.nix
@@ -163,6 +163,7 @@ in
   systemd.services.nixpkgs-update-worker2 = mkWorker "worker2";
   systemd.services.nixpkgs-update-worker3 = mkWorker "worker3";
   systemd.services.nixpkgs-update-worker4 = mkWorker "worker4";
+  systemd.services.nixpkgs-update-worker5 = mkWorker "worker5";
   # Too many workers cause out-of-memory.
 
   systemd.services.nixpkgs-update-supervisor = {
