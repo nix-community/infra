de1c033fa5e334b93b3c9a9e67626a6a22aaa7f9
zowoq <59103226+zowoq@users.noreply.github.com>
2024-12-07 10:25:44 +1000

    build01: switch to zfs_unstable

--- hosts/build01/default.nix
+++ hosts/build01/default.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 {
   imports = [
     inputs.self.nixosModules.cgroups
@@ -6,6 +6,8 @@
     inputs.self.nixosModules.disko-zfs
     inputs.srvos.nixosModules.hardware-hetzner-online-amd
   ];
+
+  boot.zfs.package = pkgs.zfs_unstable;
 
   nix.settings.max-jobs = 24;
 
