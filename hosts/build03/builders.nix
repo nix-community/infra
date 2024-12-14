512dbbf68a7981133fe8e8494861714a842311b7
zowoq <59103226+zowoq@users.noreply.github.com>
2024-12-14 13:41:56 +1000

    build03: check remote builder ssh connection

--- hosts/build03/builders.nix
+++ hosts/build03/builders.nix
@@ -1,26 +1,35 @@
 { config, inputs, ... }:
 let
   inherit (inputs.self) darwinConfigurations nixosConfigurations;
+
+  machines = [
+    darwinConfigurations.darwin02
+    nixosConfigurations.build04
+  ];
 in
 {
   sops.secrets.id_buildfarm = { };
 
   nix.distributedBuilds = true;
-  nix.buildMachines =
-    map
-      (x: {
-        hostName = "${x.config.networking.hostName}.nix-community.org";
-        maxJobs = x.config.nix.settings.max-jobs;
-        protocol = "ssh-ng";
-        sshKey = config.sops.secrets.id_buildfarm.path;
-        sshUser = "nix";
-        systems = [
-          x.config.nixpkgs.hostPlatform.system
-        ] ++ (x.config.nix.settings.extra-platforms or [ ]);
-        supportedFeatures = x.config.nix.settings.system-features;
-      })
-      [
-        darwinConfigurations.darwin02
-        nixosConfigurations.build04
-      ];
+  nix.buildMachines = map (x: {
+    hostName = "${x.config.networking.hostName}.nix-community.org";
+    maxJobs = x.config.nix.settings.max-jobs;
+    protocol = "ssh-ng";
+    sshKey = config.sops.secrets.id_buildfarm.path;
+    sshUser = "nix";
+    systems = [
+      x.config.nixpkgs.hostPlatform.system
+    ] ++ (x.config.nix.settings.extra-platforms or [ ]);
+    supportedFeatures = x.config.nix.settings.system-features;
+  }) machines;
+
+  services.telegraf.extraConfig.inputs.net_response = map (x: {
+    protocol = "tcp";
+    address = "${x.config.networking.hostName}.nix-community.org:22";
+    send = "SSH-2.0-Telegraf";
+    expect = "SSH-2.0";
+    tags.host = "${x.config.networking.hostName}.nix-community.org";
+    tags.org = "nix-community";
+    timeout = "10s";
+  }) machines;
 }
