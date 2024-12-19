518f527936b18c1ad27a4bfe43dbd7e10b7890c8
zowoq <59103226+zowoq@users.noreply.github.com>
2024-12-19 10:24:55 +1000

    modules/shared/community-builder: move secrets to sops

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -11,19 +11,11 @@ let
 
   inherit ((import ../modules/shared/known-hosts.nix).programs.ssh) knownHosts;
 
-  build01 = knownHosts.build01.publicKey;
   build02 = knownHosts.build02.publicKey;
   build03 = knownHosts.build03.publicKey;
-  darwin01 = knownHosts.darwin01.publicKey;
   web02 = knownHosts.web02.publicKey;
 
   secrets = {
-    # fine-grained, no permissions github token, expires 2025-10-29
-    # from `nix-community-buildbot` (user account, not the github app)
-    community-builder-nix-access-tokens = [
-      build01
-      darwin01
-    ];
     grafana-client-secret = [ web02 ];
     hetzner-borgbackup-ssh = [
       build02
