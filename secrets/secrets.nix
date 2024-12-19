632d80837c9edf7bc98ce7a8948885fff5323c6a
zowoq <59103226+zowoq@users.noreply.github.com>
2024-12-19 10:42:30 +1000

    web02: move secrets to sops

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -11,12 +11,7 @@ let
 
   inherit ((import ../modules/shared/known-hosts.nix).programs.ssh) knownHosts;
 
-  web02 = knownHosts.web02.publicKey;
-
   secrets = {
-    grafana-client-secret = [ web02 ];
-    nix-community-matrix-bot-token = [ web02 ];
-    oauth2-proxy-key-file = [ web02 ];
   };
 in
 builtins.listToAttrs (
