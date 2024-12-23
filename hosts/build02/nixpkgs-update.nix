85ab4e9c88bc3d940ba127a6fd7edb4ee783e400
zowoq <59103226+zowoq@users.noreply.github.com>
2024-12-24 09:10:54 +1000

    nixpkgs-update: pin nixpkgs-review

--- hosts/build02/nixpkgs-update.nix
+++ hosts/build02/nixpkgs-update.nix
@@ -24,7 +24,16 @@ let
 
     coreutils
     gist
-    nixpkgs-review
+    (nixpkgs-review.overrideAttrs (_: {
+      name = "nixpkgs-review-2.12.0";
+      version = "2.12.0";
+      src = fetchFromGitHub {
+        owner = "Mic92";
+        repo = "nixpkgs-review";
+        tag = "2.12.0";
+        hash = "sha256-yNdBqL3tceuoUHx8/j2y5ZTq1zeVDAm37RZtlCbC6rg=";
+      };
+    }))
     tree
   ];
 
