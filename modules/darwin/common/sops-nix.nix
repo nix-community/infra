ffde3228cdf2ebb8d9f92d49cd1b76131ea48312
zowoq <59103226+zowoq@users.noreply.github.com>
2025-01-03 16:18:22 +1000

    modules/darwin/common/sops-nix: disable gnupg

--- modules/darwin/common/sops-nix.nix
+++ modules/darwin/common/sops-nix.nix
@@ -4,4 +4,7 @@
     ../../shared/sops-nix.nix
     inputs.sops-nix.darwinModules.sops
   ];
+
+  # disable rsa key import, also removes gnupg packages from closure
+  sops.gnupg.sshKeyPaths = [ ];
 }
