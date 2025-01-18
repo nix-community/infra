f83732c8cc05749ad71dac8a9d67751ced66aa0f
zowoq <59103226+zowoq@users.noreply.github.com>
2025-01-18 10:09:22 +1000

    docs: refactor

--- dev/docs.nix
+++ dev/docs.nix
@@ -1,35 +1,38 @@
-{ config, pkgs, ... }:
 {
-  devShells.mkdocs = pkgs.mkShellNoCC { inputsFrom = [ config.packages.docs ]; };
-  packages = {
-    docs =
-      pkgs.runCommand "docs"
-        {
-          buildInputs = [
-            pkgs.python3.pkgs.mkdocs-material
-            pkgs.python3.pkgs.mkdocs-material.optional-dependencies.recommended
-          ];
-          files = pkgs.lib.fileset.toSource {
-            root = ../.;
-            fileset = pkgs.lib.fileset.unions [
-              ../docs
-              ../mkdocs.yml
-            ];
+  perSystem =
+    { config, pkgs, ... }:
+    {
+      devShells.mkdocs = pkgs.mkShellNoCC { inputsFrom = [ config.packages.docs ]; };
+      packages = {
+        docs =
+          pkgs.runCommand "docs"
+            {
+              buildInputs = [
+                pkgs.python3.pkgs.mkdocs-material
+                pkgs.python3.pkgs.mkdocs-material.optional-dependencies.recommended
+              ];
+              files = pkgs.lib.fileset.toSource {
+                root = ../.;
+                fileset = pkgs.lib.fileset.unions [
+                  ../docs
+                  ../mkdocs.yml
+                ];
+              };
+            }
+            ''
+              cd $files
+              mkdocs build --strict --site-dir $out
+            '';
+        docs-linkcheck = pkgs.testers.lycheeLinkCheck rec {
+          extraConfig = {
+            include_mail = true;
+            include_verbatim = true;
           };
-        }
-        ''
-          cd $files
-          mkdocs build --strict --site-dir $out
-        '';
-    docs-linkcheck = pkgs.testers.lycheeLinkCheck rec {
-      extraConfig = {
-        include_mail = true;
-        include_verbatim = true;
+          remap = {
+            "https://nix-community.org" = site;
+          };
+          site = config.packages.docs;
+        };
       };
-      remap = {
-        "https://nix-community.org" = site;
-      };
-      site = config.packages.docs;
     };
-  };
 }
