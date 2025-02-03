fc2856a54156e5035c3baa021958e9934db4d768
zowoq <59103226+zowoq@users.noreply.github.com>
2025-02-03 13:05:25 +1000

    terraform: remove unused providers

--- terraform/shell.nix
+++ terraform/shell.nix
@@ -7,9 +7,7 @@
         packages = [
           (terraform.withPlugins (p: [
             p.cloudflare
-            p.external
             p.hydra
-            p.null
             p.sops
             p.tfe
           ]))
