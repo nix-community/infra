a7ee03f5f333308eb025bf36ab7fe1401963dbde
zowoq <59103226+zowoq@users.noreply.github.com>
2025-02-07 11:48:30 +1000

    modules/nixos: remove cgroup-exporter

--- modules/nixos/cgroups.nix
+++ modules/nixos/cgroups.nix
@@ -1,20 +1,8 @@
 {
-  config,
-  inputs,
   pkgs,
   ...
 }:
 {
-  imports = [
-    inputs.cgroup-exporter.nixosModules.default
-  ];
-
-  services.prometheus.exporters.cgroup.enable = true;
-
-  services.telegraf.extraConfig.inputs.prometheus.urls = [
-    "http://localhost:${toString config.services.prometheus.exporters.cgroup.port}/metrics"
-  ];
-
   nix = {
     package = pkgs.nixVersions.nix_2_25;
 
