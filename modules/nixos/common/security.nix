94954a382c89851210c40d02104ed1bc6dd02451
zowoq <59103226+zowoq@users.noreply.github.com>
2025-03-24 09:22:05 +1000

    modules/nixos/common: remove initrd ssh
    started getting occasional initrd-nixos-copy-secrets.service failures

--- modules/nixos/common/security.nix
+++ modules/nixos/common/security.nix
@@ -1,19 +1,9 @@
-{ inputs, lib, ... }:
 {
   # Make sure that the firewall is enabled, even if it's the default.
   networking.firewall.enable = true;
 
   # allow to access emergency shell with a password
   boot.initrd.systemd.emergencyAccess = "$6$he2fblfl/H7I.kvz$WbSCMXu8ztmqfj5jG4czqvu/rkMHxufxqHgy1urzXFSN.jZB4QiW5lOjR08vk8pZTyim3TT1wFkMaNE9zZ3sc1";
-
-  boot.initrd.network = {
-    enable = true;
-    ssh = {
-      enable = true;
-      authorizedKeyFiles = lib.filesystem.listFilesRecursive "${inputs.self}/users/keys";
-      hostKeys = [ "/var/lib/ssh_secrets/initrd_host_ed25519_key" ];
-    };
-  };
 
   services.openssh = {
     hostKeys = [
