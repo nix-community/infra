00d24339064b2a930624ca1ada1a0b6fe3f5b8e8
zowoq <59103226+zowoq@users.noreply.github.com>
2025-05-01 10:22:27 +1000

    build03/builders: add isCuda, mandatoryFeatures

--- hosts/build03/builders.nix
+++ hosts/build03/builders.nix
@@ -1,4 +1,9 @@
-{ config, inputs, ... }:
+{
+  config,
+  inputs,
+  lib,
+  ...
+}:
 let
   inherit (inputs.self) darwinConfigurations nixosConfigurations;
 
@@ -11,17 +16,26 @@ in
   sops.secrets.id_buildfarm = { };
 
   nix.distributedBuilds = true;
-  nix.buildMachines = map (x: {
-    hostName = "${x.config.networking.hostName}.nix-community.org";
-    maxJobs = x.config.nix.settings.max-jobs;
-    protocol = "ssh-ng";
-    sshKey = config.sops.secrets.id_buildfarm.path;
-    sshUser = "nix";
-    systems = [
-      x.pkgs.stdenv.hostPlatform.system
-    ] ++ (x.config.nix.settings.extra-platforms or [ ]);
-    supportedFeatures = x.config.nix.settings.system-features;
-  }) machines;
+  nix.buildMachines = map (
+    x:
+    let
+      isCuda = (lib.elem "cuda" x.config.nix.settings.system-features);
+    in
+    {
+      hostName = "${x.config.networking.hostName}.nix-community.org";
+      maxJobs = if isCuda then 2 else x.config.nix.settings.max-jobs;
+      protocol = "ssh-ng";
+      sshKey = config.sops.secrets.id_buildfarm.path;
+      sshUser = "nix";
+      systems = [
+        x.pkgs.stdenv.hostPlatform.system
+      ] ++ (x.config.nix.settings.extra-platforms or [ ]);
+      supportedFeatures = x.config.nix.settings.system-features;
+    }
+    // lib.optionalAttrs isCuda {
+      mandatoryFeatures = [ "cuda" ];
+    }
+  ) machines;
 
   services.telegraf.extraConfig.inputs.net_response = map (x: {
     protocol = "tcp";
