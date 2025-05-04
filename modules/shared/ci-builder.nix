02211cc830d4dd557089e82a4c8f6540b55d590e
zowoq <59103226+zowoq@users.noreply.github.com>
2025-05-04 18:02:53 +1000

    Revert "modules/shared/ci-builder: increase max-silent-time"
    This reverts commit 3b7fb88c71dd8b8f514d1164cb7cc16321d2d06c.

--- modules/shared/ci-builder.nix
+++ modules/shared/ci-builder.nix
@@ -1,9 +1,6 @@
 {
   # match buildbot timeouts
   # https://github.com/nix-community/buildbot-nix/blob/85c0b246cc96cc244e4d9889a97c4991c4593dc3/buildbot_nix/__init__.py#L1008
-
-  # causes problems with cgroups: https://github.com/nix-community/infra/issues/1459#issuecomment-2507146996
-  nix.settings.max-silent-time = toString (60 * 20 * 3); # 3x buildbot
-
+  nix.settings.max-silent-time = toString (60 * 20);
   nix.settings.timeout = toString (60 * 60 * 3);
 }
