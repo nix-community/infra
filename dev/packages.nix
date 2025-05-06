3f8f78f7ce791ec8e82013dadb82f19eea1a35dd
zowoq <59103226+zowoq@users.noreply.github.com>
2025-05-07 09:13:44 +1000

    dev/packages: drop sk-libfido2 patch

--- dev/packages.nix
+++ dev/packages.nix
@@ -16,8 +16,6 @@
   });
   sk-libfido2 = prev.openssh.overrideAttrs (o: {
     pname = "sk-libfido2";
-    # rebase of https://github.com/openssh/openssh-portable/commit/ca0697a90e5720ba4d76cb0ae9d5572b5260a16c
-    patches = o.patches ++ [ ./sk-libfido2.patch ];
     configureFlags = o.configureFlags ++ [ "--with-security-key-standalone" ];
     buildFlags = [
       "PATHS="
@@ -26,6 +24,7 @@
     installPhase = "install sk-libfido2.dylib -Dt $out";
     postInstall = null;
     doCheck = false;
+    doInstallCheck = false;
   });
   sotp = final.buildGoModule rec {
     pname = "sotp";
