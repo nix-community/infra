b158f098deb06bb8dd5d17bbb007a6341bdc0349
zowoq <59103226+zowoq@users.noreply.github.com>
2025-05-06 20:14:39 +1000

    Revert "modules/nixos/cgroups: patch nix"
    This reverts commit 4ff161518be67c70500efe1119d97f7a26079158.

--- modules/nixos/cgroups.nix
+++ modules/nixos/cgroups.nix
@@ -1,16 +1,5 @@
-{ pkgs, ... }:
 {
   nix = {
-    package = pkgs.nix.overrideAttrs (o: {
-      patches = (o.patches or [ ]) ++ [
-        (pkgs.fetchpatch {
-          # https://github.com/NixOS/nix/pull/13135
-          url = "https://github.com/NixOS/nix/commit/7030d2e44fccc6c0fc5f5bc87eb05d7134744131.patch";
-          hash = "sha256-rsLP4OiSwQAWPO6CAehzpgnDKoPPDshzXVikUQWEmxU=";
-        })
-      ];
-    });
-
     settings = {
       experimental-features = [
         "auto-allocate-uids"
