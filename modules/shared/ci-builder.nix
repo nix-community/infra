2080948982a9e571427a2762f2addea1201ba02d
zowoq <59103226+zowoq@users.noreply.github.com>
2025-06-18 15:16:47 +1000

    modules/shared/ci-builder: limit cores to 1/4 of max-jobs

--- modules/shared/ci-builder.nix
+++ modules/shared/ci-builder.nix
@@ -1,4 +1,7 @@
+{ config, ... }:
 {
+  nix.settings.cores = config.nix.settings.max-jobs / 4;
+
   # match buildbot timeouts
   # https://github.com/nix-community/buildbot-nix/blob/85c0b246cc96cc244e4d9889a97c4991c4593dc3/buildbot_nix/__init__.py#L1008
 
