8a19858aace2a1715fb19079845cd2c5acbfe60e
zowoq <59103226+zowoq@users.noreply.github.com>
2025-07-14 09:45:17 +1000

    terraform/moved: DROP

---
+++ terraform/moved.tf
@@ -0,0 +1,54 @@
+moved {
+  from = hydra_jobset.nixpkgs_jobset["bsd"]
+  to   = hydra_jobset.bsd
+}
+
+moved {
+  from = hydra_jobset.nixpkgs_jobset["bsd_staging_next"]
+  to   = hydra_jobset.bsd_staging_next
+}
+
+moved {
+  from = hydra_jobset.nixpkgs_jobset["cuda"]
+  to   = hydra_jobset.cuda
+}
+
+moved {
+  from = hydra_jobset.nixpkgs_jobset["cuda_stable"]
+  to   = hydra_jobset.cuda_stable
+}
+
+moved {
+  from = hydra_jobset.nixpkgs_jobset["cuda_stable_previous"]
+  to   = hydra_jobset.cuda_stable_previous
+}
+
+moved {
+  from = hydra_jobset.nixpkgs_jobset["rocm"]
+  to   = hydra_jobset.rocm
+}
+
+moved {
+  from = hydra_jobset.nixpkgs_jobset["unfree_redist"]
+  to   = hydra_jobset.unfree_redist
+}
+
+moved {
+  from = hydra_jobset.nixpkgs_jobset["unfree_redist_darwin"]
+  to   = hydra_jobset.unfree_redist_darwin
+}
+
+moved {
+  from = hydra_jobset.nixpkgs_jobset["unfree_redist_full"]
+  to   = hydra_jobset.unfree_redist_full
+}
+
+moved {
+  from = hydra_jobset.nixpkgs_jobset["unfree_redist_stable"]
+  to   = hydra_jobset.unfree_redist_stable
+}
+
+moved {
+  from = hydra_jobset.nixpkgs_jobset["unfree_redist_stable_previous"]
+  to   = hydra_jobset.unfree_redist_stable_previous
+}
