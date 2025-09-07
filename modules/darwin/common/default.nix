d644ac9e9da56bb58b1a5afb40e865d2cf6c0034
zowoq <59103226+zowoq@users.noreply.github.com>
2025-09-07 10:43:04 +1000

    modules/darwin/common: apply kqueue patch to nix.package

--- modules/darwin/common/default.nix
+++ modules/darwin/common/default.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 {
   imports = [
     ../../shared/known-hosts.nix
@@ -14,6 +14,16 @@
     ./users.nix
     inputs.srvos.darwinModules.server
   ];
+
+  nix.package = pkgs.nix.overrideAttrs (o: {
+    patches = o.patches or [ ] ++ [
+      (pkgs.fetchpatch {
+        # Fix macOS HUP detection using kqueue instead of poll
+        url = "https://github.com/NixOS/nix/commit/1286d5db78701a5c0a83ae6b5f838b9ac60a61c1.patch";
+        hash = "sha256-zJNzMyFf5Jq+UTYHx3AZl6MVc2I5CKk3YwvcZvuG2bE=";
+      })
+    ];
+  });
 
   # https://github.com/NixOS/nix/issues/7273
   nix.settings.auto-optimise-store = false;
