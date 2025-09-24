bafc521722eb66921dd03e5457afd7f14b18360a
zowoq <59103226+zowoq@users.noreply.github.com>
2025-09-25 08:44:54 +1000

    flake: pin buildbot-nix

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     buildbot-nix.inputs.hercules-ci-effects.follows = "hercules-ci-effects";
     buildbot-nix.inputs.nixpkgs.follows = "nixpkgs";
     buildbot-nix.inputs.treefmt-nix.follows = "treefmt-nix";
-    buildbot-nix.url = "github:nix-community/buildbot-nix";
+    buildbot-nix.url = "github:nix-community/buildbot-nix/82745470e3fa50e1bdcc8f59745121aa29f1ac7a";
     disko.inputs.nixpkgs.follows = "nixpkgs";
     disko.url = "github:nix-community/disko";
     empty.url = "github:nix-systems/empty";
