c5a99ca50c812657d936ee65f12008f7cd48a36b
zowoq <59103226+zowoq@users.noreply.github.com>
2025-09-27 09:40:25 +1000

    Revert "modules/darwin/common: fix telegraf 1.36.1"
    This reverts commit 719c775655509cb47eb37b9b0e13a7982b92db6f.

--- modules/darwin/common/telegraf.nix
+++ modules/darwin/common/telegraf.nix
@@ -4,7 +4,4 @@
     inputs.srvos.darwinModules.mixins-telegraf
     ../../shared/telegraf.nix
   ];
-
-  # https://github.com/influxdata/telegraf/issues/17607
-  launchd.daemons.telegraf.environment.HOME = "/var/root";
 }
