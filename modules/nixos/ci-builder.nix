273a397d624c19d21dff8d17b16512cad453737c
zowoq <59103226+zowoq@users.noreply.github.com>
2025-12-05 17:44:25 +1000

    Revert "switch to new queue runner"
    This reverts commit 7181398761226ec8b82ab34e6dc6f557f86110a9.

--- modules/nixos/ci-builder.nix
+++ modules/nixos/ci-builder.nix
@@ -1,24 +1,3 @@
 {
-  config,
-  inputs,
-  ...
-}:
-{
-  imports = [
-    ../shared/ci-builder.nix
-    "${inputs.self}/modules/queue-runner/hydra-queue-builder-v2.nix"
-  ];
-
-  sops.secrets.queue-runner-client-key.owner = "hydra-queue-builder";
-
-  nixCommunity.hydra-queue-builder-v2 = {
-    enable = true;
-    queueRunnerAddr = "https://queue-runner.hydra.nix-community.org";
-    mtls = {
-      serverRootCaCertPath = "${../../hosts/build03/ca.crt}";
-      clientCertPath = "${../../hosts/${config.networking.hostName}/client.crt}";
-      clientKeyPath = config.sops.secrets.queue-runner-client-key.path;
-      domainName = "queue-runner.hydra.nix-community.org";
-    };
-  };
+  imports = [ ../shared/ci-builder.nix ];
 }
