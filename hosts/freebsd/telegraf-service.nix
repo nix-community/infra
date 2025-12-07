7faecd861cc22302595abca4f9502d05bb75f0df
zowoq <59103226+zowoq@users.noreply.github.com>
2025-12-07 16:56:37 +1000

    update freebsd VMs to 15

--- hosts/freebsd/telegraf-service.nix
+++ hosts/freebsd/telegraf-service.nix
@@ -13,26 +13,7 @@ let
   settingsFormat = pkgs.formats.toml { };
   configFile = settingsFormat.generate "config.toml" cfg.extraConfig;
 
-  package = pkgs.buildGo124Module rec {
-    pname = "telegraf";
-    version = "1.34.4";
-    subPackages = [ "cmd/telegraf" ];
-    src = pkgs.fetchFromGitHub {
-      owner = "influxdata";
-      repo = "telegraf";
-      rev = "v${version}";
-      hash = "sha256-oFhSCBGS8brXBxUiuXTCQiwRWuLvjDPte2Zi6BwelJs=";
-    };
-    env.CGO_ENABLED = 0;
-    vendorHash = "sha256-C4p+dZkudSIJI4036RR5J8rokEUB1Vi+xTC6Ijf9gUc=";
-    proxyVendor = true;
-    ldflags = [
-      "-s"
-      "-w"
-      "-X=github.com/influxdata/telegraf/internal.Commit=${src.rev}"
-      "-X=github.com/influxdata/telegraf/internal.Version=${version}"
-    ];
-  };
+  package = pkgs.telegraf;
 in
 {
   options.services.telegraf = {
