50ccbce653a8a62ca98a887c090e1f053a520405
Luke Bailey <156000062+baileylu121@users.noreply.github.com>
2025-12-07 15:19:16 +0000

    modules/nixos/buildbot.nix: Add `bun2nix`
    Adds `bun2nix` to the allowed repos for buildbot

--- modules/nixos/buildbot.nix
+++ modules/nixos/buildbot.nix
@@ -8,6 +8,7 @@ let
   repoAllowlist = [
     # keep-sorted start case=no
     "nix-community/authentik-nix"
+    "nix-community/bun2nix"
     "nix-community/dream2nix"
     "nix-community/ethereum.nix"
     "nix-community/home-manager"
