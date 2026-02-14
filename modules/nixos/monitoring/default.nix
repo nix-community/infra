1b3331fcd10d360b2ba01574df7132c3c074237e
zowoq <59103226+zowoq@users.noreply.github.com>
2026-02-14 14:31:37 +1000

    modules/nixos/monitoring: remove grafana

--- modules/nixos/monitoring/default.nix
+++ modules/nixos/monitoring/default.nix
@@ -3,7 +3,6 @@
   imports = [
     inputs.srvos.nixosModules.roles-prometheus
     ./alert-rules.nix
-    ./grafana.nix
     ./matrix-hook.nix
     ./oauth2-proxy.nix
     ./prometheus.nix
@@ -12,12 +11,6 @@
 
   services.nginx.virtualHosts."alertmanager.nix-community.org" = {
     locations."/".proxyPass = "http://localhost:9093/";
-  };
-  services.nginx.virtualHosts."grafana.nix-community.org" = {
-    locations."/" = {
-      proxyPass = "http://localhost:3000/";
-      proxyWebsockets = true;
-    };
   };
   services.nginx.virtualHosts."prometheus.nix-community.org" = {
     locations."/".proxyPass = "http://localhost:9090/";
