57dbd8dd3b02aaf50dd15d3eddb49fd1bdaf9d79
zowoq <59103226+zowoq@users.noreply.github.com>
2026-06-27 11:55:51 +1000

    darwin: set extra-platforms explicitly

--- hosts/darwin01/default.nix
+++ hosts/darwin01/default.nix
@@ -10,6 +10,7 @@
   nixCommunity.darwin.ipv6 = "2a01:4f8:d1:5716::2 64 2a01:4f8:d1:5716::1";
 
   nix.settings.sandbox = "relaxed";
+  nix.settings.extra-platforms = [ ];
 
   # disable nixos-tests
   nix.settings.system-features = [ "big-parallel" ];
