4037c86dab3425276e42d2f5c344f3f20ddf43c2
zowoq <59103226+zowoq@users.noreply.github.com>
2026-07-11 13:09:14 +1000

    terraform/hydra-projects: remove kittybox
    upstream project hasn't been active for a while

--- terraform/hydra-projects.tf
+++ terraform/hydra-projects.tf
@@ -1,32 +1,5 @@
 # See https://github.com/DeterminateSystems/terraform-provider-hydra for explanation
 
-resource "hydra_project" "kittybox" {
-  name         = "kittybox"
-  display_name = "Kittybox"
-  description  = "The IndieWeb blogging solution"
-  homepage     = "https://sr.ht/~vikanezrimaya/kittybox"
-  owner        = "admin"
-  enabled      = true
-  visible      = true
-}
-
-resource "hydra_jobset" "kittybox" {
-  project     = hydra_project.kittybox.name
-  state       = "enabled"
-  visible     = true
-  name        = "main"
-  type        = "flake"
-  description = "main branch"
-
-  flake_uri = "git+https://git.sr.ht/~vikanezrimaya/kittybox?ref=main"
-
-  check_interval    = 1800
-  scheduling_shares = 3000
-  keep_evaluations  = 3
-
-  email_notifications = false
-}
-
 resource "hydra_project" "emacs_overlay" {
   name         = "emacs-overlay"
   display_name = "Emacs Overlay"
