8357183d024a5919c213a87543c84b40707f9b94
zowoq <59103226+zowoq@users.noreply.github.com>
2026-07-27 13:51:05 +1000

    dev/effect-deploy: add effect-is-buildable check

--- dev/effect-deploy.nix
+++ dev/effect-deploy.nix
@@ -1,33 +1,49 @@
-{ self, withSystem, ... }:
 {
+  config,
+  lib,
+  self,
+  withSystem,
+  ...
+}:
+let
+  inherit (config) defaultEffectSystem;
+  inherit ((import "${self}/modules/shared/known-hosts.nix").programs.ssh) knownHosts;
+
+  targets = builtins.attrNames self.darwinConfigurations;
+
+  buildable = effect: (effect.overrideAttrs { isEffect = false; }).inputDerivation;
+
+  deploy = x: {
+    ssh.destination = "customer@${x}.nix-community.org";
+    configuration = self.darwinConfigurations.${x};
+    secretsMap.ssh-deployment = "ssh-deployment";
+    userSetupScript = ''
+      writeSSHKey ssh-deployment
+      cat >> ~/.ssh/known_hosts <<EOF
+      ${toString knownHosts.${x}.hostNames} ${knownHosts.${x}.publicKey}
+      EOF
+    '';
+  };
+in
+{
+  perSystem =
+    { hci-effects, system, ... }:
+    lib.optionalAttrs (system == defaultEffectSystem) {
+      checks.darwin-effect-is-buildable = buildable (
+        hci-effects.runNixDarwin (deploy (lib.last targets))
+      );
+    };
+
   herculesCI = herculesCI: {
-    onPush.default.outputs.effects = withSystem "x86_64-linux" (
+    onPush.default.outputs.effects = withSystem defaultEffectSystem (
       { hci-effects, ... }:
-      let
-        hosts = (import "${self}/modules/shared/known-hosts.nix").programs.ssh.knownHosts;
-      in
-      builtins.listToAttrs (
-        map
-          (x: {
-            name = x;
-            value = hci-effects.runIf (herculesCI.config.repo.ref == "refs/heads/master") (
-              hci-effects.runNixDarwin {
-                ssh.destination = "customer@${x}.nix-community.org";
-                configuration = self.darwinConfigurations.${x};
-                secretsMap.ssh-deployment = "ssh-deployment";
-                userSetupScript = ''
-                  writeSSHKey ssh-deployment
-                  cat >>~/.ssh/known_hosts <<EOF
-                  ${toString hosts.${x}.hostNames} ${hosts.${x}.publicKey}
-                  EOF
-                '';
-              }
-            );
-          })
-          [
-            "darwin01"
-            "darwin02"
-          ]
+      lib.listToAttrs (
+        map (x: {
+          name = x;
+          value = hci-effects.runIf (herculesCI.config.repo.ref == "refs/heads/master") (
+            hci-effects.runNixDarwin (deploy x)
+          );
+        }) targets
       )
     );
   };
