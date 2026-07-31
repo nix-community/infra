054cf8794a88fad14433e873a34ca57de58ef06a
zowoq <59103226+zowoq@users.noreply.github.com>
2026-07-31 14:13:30 +1000

    modules/shared/ci-builder: update timeout comments and link

--- modules/shared/ci-builder.nix
+++ modules/shared/ci-builder.nix
@@ -2,11 +2,11 @@
 {
   nix.settings.cores = config.nix.settings.max-jobs / 4;
 
-  # match buildbot timeouts
-  # https://github.com/nix-community/buildbot-nix/blob/85c0b246cc96cc244e4d9889a97c4991c4593dc3/buildbot_nix/__init__.py#L1008
+  # match nixbot timeouts
+  # https://github.com/Mic92/nixbot/blob/6764a0ef1c704b6db339f19d81cb5642a2508dff/nixosModules/nixbot.nix#L264
 
   # causes problems with cgroups: https://github.com/nix-community/infra/issues/1459#issuecomment-2507146996
-  nix.settings.max-silent-time = toString (60 * 20 * 3); # 3x buildbot
+  nix.settings.max-silent-time = toString (60 * 20 * 3); # 3x nixbot
 
   nix.settings.timeout = toString (60 * 60 * 3);
 }
