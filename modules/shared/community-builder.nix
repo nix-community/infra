41d3c08c53c5bd4b37081081c9ff78cc80a1589d
zowoq <59103226+zowoq@users.noreply.github.com>
2026-08-02 09:12:24 +1000

    modules/shared/community-builder: fix emacs on darwin

--- modules/shared/community-builder.nix
+++ modules/shared/community-builder.nix
@@ -46,7 +46,7 @@
     # useful for people that want to test stuff
     environment.systemPackages = [
       pkgs.btop
-      pkgs.emacs
+      (pkgs.emacs.override { withMailutils = !pkgs.stdenv.isDarwin; })
       pkgs.fd
       pkgs.git
       pkgs.nano
