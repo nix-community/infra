0c83fff554137469d6996efbee8d7cfb0f68685a
zowoq <59103226+zowoq@users.noreply.github.com>
2026-09-05 08:27:14 +1000

    dev/effect-repo-list: remove is-buildable check

--- dev/effect-repo-list.nix
+++ dev/effect-repo-list.nix
@@ -5,47 +5,30 @@
   ...
 }:
 let
-  inherit (config) defaultEffectSystem;
-
   commit = "devdoc: update repo list";
-
-  buildable = effect: (effect.overrideAttrs { isEffect = false; }).inputDerivation;
-
-  effect =
-    hci-effects: gitRemote:
-    hci-effects.flakeUpdate {
-      inherit gitRemote;
-      createPullRequest = true;
-      pullRequestTitle = commit;
-      baseMergeBranch = "master";
-      baseMergeMethod = "reset";
-      updateBranch = "update-repo-list";
-      module.git.update.script = lib.mkForce ''
-        gh api --paginate /orgs/nix-community/repos --jq '.[].html_url' | sort --ignore-case > devdoc/repo_list
-        git add devdoc/repo_list
-        if git diff --cached --quiet; then
-          echo "Nothing to commit."
-        else
-          git commit -m "${commit}"
-        fi
-      '';
-    };
 in
 {
-  perSystem =
-    { hci-effects, system, ... }:
-    lib.optionalAttrs (system == defaultEffectSystem) {
-      # this check ensures that the hercules-ci.flake-update and update-repo-list effects are buildable
-      checks.on-schedule-pull-request-effect-is-buildable = buildable (
-        effect hci-effects "https://fake-repo-for.checks.update-repo-list-effect-is-buildable"
-      );
-    };
-
   herculesCI = herculesCI: {
     onSchedule.update-repo-list = {
-      outputs.effects = withSystem defaultEffectSystem (
+      outputs.effects = withSystem config.defaultEffectSystem (
         { hci-effects, ... }: {
-          update-repo-list = effect hci-effects herculesCI.config.repo.remoteHttpUrl;
+          update-repo-list = hci-effects.flakeUpdate {
+            gitRemote = herculesCI.config.repo.remoteHttpUrl;
+            createPullRequest = true;
+            pullRequestTitle = commit;
+            baseMergeBranch = "master";
+            baseMergeMethod = "reset";
+            updateBranch = "update-repo-list";
+            module.git.update.script = lib.mkForce ''
+              gh api --paginate /orgs/nix-community/repos --jq '.[].html_url' | sort --ignore-case > devdoc/repo_list
+              git add devdoc/repo_list
+              if git diff --cached --quiet; then
+                echo "Nothing to commit."
+              else
+                git commit -m "${commit}"
+              fi
+            '';
+          };
         }
       );
       when.hour = [ 2 ];
