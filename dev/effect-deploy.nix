7a87f050bc50e0416466ab67932257ac078f528d
zowoq <59103226+zowoq@users.noreply.github.com>
2026-09-05 08:27:14 +1000

    dev/effect-deploy: remove is-buildable check

--- dev/effect-deploy.nix
+++ dev/effect-deploy.nix
@@ -6,44 +6,29 @@
   ...
 }:
 let
-  inherit (config) defaultEffectSystem;
   inherit ((import "${self}/modules/shared/known-hosts.nix").programs.ssh) knownHosts;
-
-  targets = builtins.attrNames self.darwinConfigurations;
-
-  buildable = effect: (effect.overrideAttrs { isEffect = false; }).inputDerivation;
-
-  deploy = x: {
-    ssh.destination = "customer@${x}.nix-community.org";
-    configuration = self.darwinConfigurations.${x};
-    secretsMap.ssh-deployment = "ssh-deployment";
-    userSetupScript = ''
-      writeSSHKey ssh-deployment
-      cat >> ~/.ssh/known_hosts <<EOF
-      ${toString knownHosts.${x}.hostNames} ${knownHosts.${x}.publicKey}
-      EOF
-    '';
-  };
 in
 {
-  perSystem =
-    { hci-effects, system, ... }:
-    lib.optionalAttrs (system == defaultEffectSystem) {
-      checks.darwin-effect-is-buildable = buildable (
-        hci-effects.runNixDarwin (deploy (lib.last targets))
-      );
-    };
-
   herculesCI = herculesCI: {
-    onPush.default.outputs.effects = withSystem defaultEffectSystem (
+    onPush.default.outputs.effects = withSystem config.defaultEffectSystem (
       { hci-effects, ... }:
       lib.listToAttrs (
         map (x: {
           name = x;
           value = hci-effects.runIf (herculesCI.config.repo.ref == "refs/heads/master") (
-            hci-effects.runNixDarwin (deploy x)
+            hci-effects.runNixDarwin {
+              ssh.destination = "customer@${x}.nix-community.org";
+              configuration = self.darwinConfigurations.${x};
+              secretsMap.ssh-deployment = "ssh-deployment";
+              userSetupScript = ''
+                writeSSHKey ssh-deployment
+                cat >> ~/.ssh/known_hosts <<EOF
+                ${toString knownHosts.${x}.hostNames} ${knownHosts.${x}.publicKey}
+                EOF
+              '';
+            }
           );
-        }) targets
+        }) (builtins.attrNames self.darwinConfigurations)
       )
     );
   };
